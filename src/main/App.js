e2437fe7474445765ca12b57bd2852025d8497ec
g.zacharski <g.zacharski@wp.pl>
2021-03-02 23:13:54 +0100

    Added CssBaseline component

--- src/main/App.js
+++ src/main/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import { Footer, Header, Navigation, Page } from "./layout";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function App() {
   return (
     <Fragment>
+      <CssBaseline/>
       <Header />
       <Navigation />
       <Page />
