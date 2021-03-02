a34f01a9593d9473e5f15acfbcb134394417ce2e
g.zacharski <g.zacharski@wp.pl>
2021-03-02 19:03:20 +0100

    Changed footer content and added its tests.

--- src/main/layout/Footer.js
+++ src/main/layout/Footer.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles=makeStyles({
+    root:{
+        padding: "50px",
+    },
+    footer_title:{
+        fontWeight: "bold",
+    }
+})
 
 export default function Footer(){
+    const classes=useStyles();
     return(
-        <footer>
-            <h2>Footer</h2>
-        </footer>
+        <Container maxWidth='xl' component='footer' className={classes.root} data-testid='footer-container' >
+            <Typography align='center' component='h6' className={classes.footer_title} data-testid='footer-title'>
+                System do wspomagania zarządzania placówką profilaktyki zdrowotnej
+            </Typography>
+            <Typography align='center' component='p' data-testid='footer-authors'>
+                Bartosz Kordek Grzegorz Zacharski
+            </Typography>
+            <Typography align='center' component='p' data-testid='footer-year'>
+                2020/2021
+            </Typography>
+        </Container>
     );
 }
--- src/main/layout/Footer.test.js
+++ src/main/layout/Footer.test.js
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("Footer renders ", () => {
+describe("Footer renders", () => {
   test("wihout crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Footer />, div);
@@ -15,9 +15,36 @@ describe("Footer renders ", () => {
   });
 });
 
-describe('Footer component contains: ',()=>{
-    test('text "Footer"', () => {
-      render(<Footer />);
-      expect(screen.getByText('Footer')).toBeInTheDocument();
+describe('Footer component',()=>{
+    beforeEach(()=>{
+      render(<Footer/>);
+    })
+
+    test('contains testId equal footer-container.',()=>{
+      expect(screen.getByTestId('footer-container')).toBeInTheDocument();
+      expect(screen.getByTestId('footer-container')).toBeVisible();
+    })
+
+    test('contains title.',()=>{
+      expect(screen.getByTestId('footer-title')).toBeInTheDocument();
+      expect(screen.getByTestId('footer-title')).toBeVisible();
+    })
+
+    test.each([
+      'System do wspomagania zarządzania placówką profilaktyki zdrowotnej'
+    ])('title is %s',(expectedTitle)=>{
+      expect(screen.getByTestId('footer-title').textContent).toBe(expectedTitle);
+    })
+
+    test('contains authors.',()=>{
+      expect(screen.getByTestId('footer-authors')).toBeInTheDocument();
+    })
+
+    test.each([
+      'Bartosz Kordek',
+      'Grzegorz Zacharski'
+    ])('contains author %s.',(author)=>{
+      expect(screen.getByTestId('footer-authors')).toHaveTextContent(author);
+      expect(screen.getByTestId('footer-authors')).toBeVisible();
     })
 })
