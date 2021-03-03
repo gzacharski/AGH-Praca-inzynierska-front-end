8a59b165d489a3fcc16a8fbdd4391eb920deedc2
g.zacharski <g.zacharski@wp.pl>
2021-03-03 01:00:20 +0100

    Created initial pages.

---
+++ src/main/pages/Trenings.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles=makeStyles({
+    root : {
+        fontWeight: 'bold',
+        padding: '30px'
+    }
+})
+
+export default function Trenings(){
+    const classes=useStyles();
+    return(
+        <Typography variant='h5' className={classes.root} align='center'>
+            Trening
+        </Typography>
+    );
+}
---
+++ src/main/pages/index.js
@@ -0,0 +1,10 @@
+import About from './About';
+import Blog from "./Blog";
+import Client from './Client';
+import Contact from './Contact';
+import Home from './Home';
+import Login from './Login';
+import News from './News';
+import Offer from './Offer';
+
+export { About, Blog, Client, Contact, Home, Login, News, Offer };
