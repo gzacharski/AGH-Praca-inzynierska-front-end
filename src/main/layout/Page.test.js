9cb1b0c5db8ac607a64d1f81b7e10c94bd6a4871
g.zacharski <g.zacharski@wp.pl>
2021-03-05 22:21:10 +0100

    Overwritten render function added instead of native RTL function.

--- src/main/layout/Page.test.js
+++ src/main/layout/Page.test.js
@@ -1,35 +1,35 @@
 import React from "react";
-import {MemoryRouter} from "react-router-dom"
-import {render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom"
+import { render, screen } from '../../testUtils';
 import Page from "./Page";
-import { About, Blog, Client, Contact, Home, Login, News, Offer} from '../pages';
+import { About, Blog, Client, Contact, Home, Login, News, Offer } from '../pages';
 
-jest.mock('../pages/About');
-jest.mock('../pages/Blog');
-jest.mock('../pages/Client');
-jest.mock('../pages/Contact');
-jest.mock('../pages/Home');
-jest.mock('../pages/Login');
-jest.mock('../pages/News');
-jest.mock('../pages/Offer');
+jest.mock('../pages/About', () => jest.fn());
+jest.mock('../pages/Blog', () => jest.fn());
+jest.mock('../pages/Client', () => jest.fn());
+jest.mock('../pages/Contact', () => jest.fn());
+jest.mock('../pages/Home', () => jest.fn());
+jest.mock('../pages/Login', () => jest.fn());
+jest.mock('../pages/News', () => jest.fn());
+jest.mock('../pages/Offer', () => jest.fn());
 
 describe("Page", () => {
 
   test.each([
-    ['/about',About],
-    ['/blog',Blog],
-    ['/client',Client],
-    ['/contact',Contact],
-    ['/',Home],
-    ['/login',Login],
-    ['/news',News],
-    ['/offer',Offer]
-  ])('with valid path: "%s" should redirect to %p page.',(link, component)=>{
-    component.mockImplementation(()=><div data-testid={component.name}>{component.name}</div>);
+    ['/about', About],
+    ['/blog', Blog],
+    ['/client', Client],
+    ['/contact', Contact],
+    ['/', Home],
+    ['/login', Login],
+    ['/news', News],
+    ['/offer', Offer]
+  ])('with valid path: "%s" should redirect to %p page.', (link, component) => {
+    component.mockImplementation(() => <div data-testid={component.name}>{component.name}</div>);
 
     render(
       <MemoryRouter initialEntries={[link]}>
-        <Page/>
+        <Page />
       </MemoryRouter>
     );
 
@@ -37,23 +37,23 @@ describe("Page", () => {
     expect(screen.queryByTestId(`${component.name}`)).toBeInTheDocument();
     expect(screen.queryByTestId("randomId1234")).not.toBeInTheDocument();
   })
-  
+
   test.each([
-    ['/about123',About],
-    ['/blog1',Blog],
-    ['/client3',Client],
-    ['/contact4',Contact],
-    ['/fafa',Contact],
-    ['/loginda',Login],
-    ['/newsfa',News],
-    ['/offerf',Offer]
-  ])('with invalid path: "%s" should redirect to Home page.',(link,component)=>{
-    Home.mockImplementation(()=><div data-testid="home">Home</div>);
-    component.mockImplementation(()=><div data-testid={component.name}>{component.name}</div>);
+    ['/about123', About],
+    ['/blog1', Blog],
+    ['/client3', Client],
+    ['/contact4', Contact],
+    ['/fafa', Contact],
+    ['/loginda', Login],
+    ['/newsfa', News],
+    ['/offerf', Offer]
+  ])('with invalid path: "%s" should redirect to Home page.', (link, component) => {
+    Home.mockImplementation(() => <div data-testid="home">Home</div>);
+    component.mockImplementation(() => <div data-testid={component.name}>{component.name}</div>);
 
     render(
       <MemoryRouter initialEntries={[link]}>
-        <Page/>
+        <Page />
       </MemoryRouter>
     );
 
@@ -62,12 +62,12 @@ describe("Page", () => {
     expect(screen.queryByTestId(`${component.name}`)).toBeFalsy();
   })
 
-  test('contains div with data-testid main-container',()=>{
-    Home.mockImplementation(()=><div data-testid="home">Home</div>);
+  test('contains div with data-testid main-container', () => {
+    Home.mockImplementation(() => <div data-testid="home">Home</div>);
 
     render(
       <MemoryRouter>
-        <Page/>
+        <Page />
       </MemoryRouter>
     );
 
