ee02e6e42353cd0caa3fcfcc45b5c2b342149819
g.zacharski <g.zacharski@wp.pl>
2021-03-05 16:41:15 +0100

    Bug fixed in state reducer.

--- src/main/store/state/reducer.js
+++ src/main/store/state/reducer.js
@@ -1,17 +1,14 @@
 import * as ActionTypes from './action/types';
 import { initialStore } from '../initialStore';
 
-export default function reducer(store, action) {
+export default function reducer(stateData, action) {
     switch (action.type) {
         case ActionTypes.TOGGLE_DRAWER:
             return {
-                ...store,
-                stateData:{
-                    ...store.stateData,
-                    menuIsOpen: !store.stateData.menuIsOpen
+                    ...stateData,
+                    menuIsOpen: !stateData.menuIsOpen
                 }
-            }
         default:
-            return store || initialStore;
+            return stateData || initialStore.stateData;
     }
 }
