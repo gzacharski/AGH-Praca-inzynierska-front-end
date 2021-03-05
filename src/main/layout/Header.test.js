407299073112c8843be19f90b9d52c9592d822cb
g.zacharski <g.zacharski@wp.pl>
2021-03-05 22:18:42 +0100

    Enzyme test removed. Overwritten render function added.

--- src/main/layout/Header.test.js
+++ src/main/layout/Header.test.js
@@ -1,50 +1,37 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { render, screen } from "@testing-library/react";
-import Adapter from "enzyme-adapter-react-16";
-import Enzyme, { shallow } from "enzyme";
+import { render, screen } from '../../testUtils';
 import Header from "./Header";
 
-Enzyme.configure({ adapter: new Adapter() });
+describe('Header component', () => {
+  beforeEach(() => {
+    render(<Header />);
+  })
 
-describe("Header renders", () => {
-  test("wihout crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Header />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  test('contains tag "header"', () => {
+    expect(screen.getByRole('header')).toBeInTheDocument();
+  })
+
+  test('contains visible menu icon', () => {
+    expect(screen.getByTestId('header-menu-icon')).toBeInTheDocument();
+    expect(screen.getByTestId('header-menu-icon')).toBeVisible();
+  })
+
+  test('contains visible title on app bar', () => {
+    expect(screen.getByTestId('header-menu-title')).toBeInTheDocument();
+    expect(screen.getByTestId('header-menu-title')).toBeVisible();
+  })
+
+  test('contains visible search component', () => {
+    expect(screen.getByRole('search')).toBeInTheDocument();
+    expect(screen.getByRole('search')).toBeVisible();
+  })
+
+  test('contains visible log in button', () => {
+    expect(screen.getByTestId('header-login-button')).toBeInTheDocument();
+    expect(screen.getByTestId('header-login-button')).toBeVisible();
   });
-});
 
-describe('Header component',()=>{
-    beforeEach(()=>{
-      render(<Header/>);
-    })
-
-    test('contains tag "header"', () => {
-      expect(screen.getByRole('header')).toBeInTheDocument();
-    })
-
-    test('contains visible menu icon',()=>{
-      expect(screen.getByTestId('header-menu-icon')).toBeInTheDocument();
-      expect(screen.getByTestId('header-menu-icon')).toBeVisible();
-    })
-
-    test('contains visible title on app bar',()=>{
-      expect(screen.getByTestId('header-menu-title')).toBeInTheDocument();
-      expect(screen.getByTestId('header-menu-title')).toBeVisible();
-    })
-
-    test('contains visible search component',()=>{
-      expect(screen.getByRole('search')).toBeInTheDocument();
-      expect(screen.getByRole('search')).toBeVisible(); 
-    })
-
-    test('contains visible log in button',()=>{
-      expect(screen.getByTestId('header-login-button')).toBeInTheDocument();
-      expect(screen.getByTestId('header-login-button')).toBeVisible();
-    });
-
-    test('contains search input',()=>{
-      expect(screen.getByRole('search')).toBeInTheDocument();
-    })
+  test('contains search input', () => {
+    expect(screen.getByRole('search')).toBeInTheDocument();
+  })
 })
