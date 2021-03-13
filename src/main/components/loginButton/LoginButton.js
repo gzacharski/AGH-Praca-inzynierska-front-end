ea838bfb54d39fc66856983e2c2444673ab7956e
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 23:33:23 +0100

    Add style file to LoginButton

--- src/main/components/loginButton/LoginButton.js
+++ src/main/components/loginButton/LoginButton.js
@@ -1,15 +1,17 @@
 import Button from "@material-ui/core/Button";
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { useStyles } from "./LoginButton.styles";
 
 const LoginButton = (props) => {
+  const classes=useStyles();
   const handleClick = (history) => history.push("/login");
   return (
     <Button
-      color="inherit"
       data-testid="header-login-button"
       role="button"
       onClick={() => handleClick(props.history)}
+      className={classes.root}
     >
       Zaloguj się
     </Button>
