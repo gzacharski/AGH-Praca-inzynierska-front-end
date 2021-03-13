ff9bc5a58bb6d425616bf2cd5241a4636391a026
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 22:54:11 +0100

    Remove 'Zaloguj się' listItem. Format code.

--- src/main/layout/Navigation.js
+++ src/main/layout/Navigation.js
@@ -1,51 +1,46 @@
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { toggleDrawer } from "../store/state/action/creators";
-import { NavLink } from 'react-router-dom';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
 const Navigation = (props) => {
-    return (
-        <nav>
-            <Drawer open={props.menuIsOpen} onClose={() => props.toggleDrawer()}>
-                <List component='nav' aria-label='About'>
-                    <ListItem button component={NavLink} to="/" exact={true}>
-                        <ListItemText>Strona główna</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/news">
-                        <ListItemText>Aktualności</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/about">
-                        <ListItemText>O nas</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/blog">
-                        <ListItemText>Blog</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/client">
-                        <ListItemText>Klient</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/contact">
-                        <ListItemText>Kontakt</ListItemText>
-                    </ListItem>
-                    <ListItem button component={NavLink} to="/offer">
-                        <ListItemText>Oferta</ListItemText>
-                    </ListItem>
-                    <Divider />
-                    <ListItem button component={NavLink} to="/login">
-                        <ListItemText>Zaloguj się</ListItemText>
-                    </ListItem>
-                </List>
-            </Drawer>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <Drawer open={props.menuIsOpen} onClose={() => props.toggleDrawer()}>
+        <List component="nav" aria-label="About">
+          <ListItem button component={NavLink} to="/" exact={true}>
+            <ListItemText>Strona główna</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/news">
+            <ListItemText>Aktualności</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/about">
+            <ListItemText>O nas</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/blog">
+            <ListItemText>Blog</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/client">
+            <ListItemText>Klient</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/contact">
+            <ListItemText>Kontakt</ListItemText>
+          </ListItem>
+          <ListItem button component={NavLink} to="/offer">
+            <ListItemText>Oferta</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+    </nav>
+  );
+};
 
-const mapStateToProps = (store) => ({ menuIsOpen: store.stateData.menuIsOpen })
+const mapStateToProps = (store) => ({ menuIsOpen: store.stateData.menuIsOpen });
 
-const mapDispatchToProps = { toggleDrawer }
+const mapDispatchToProps = { toggleDrawer };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
