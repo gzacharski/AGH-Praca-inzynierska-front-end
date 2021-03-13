b4989ee34ed9c4b71678d726a8c4f903f7f267dc
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 13:31:47 +0100

    'Client' comp.: add styles file and add tests.

---
+++ src/main/pages/client/Client.styles.js
@@ -0,0 +1,10 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles=makeStyles({
+    root : {
+        fontWeight: 'bold',
+        padding: '30px'
+    }
+})
+
+export { useStyles };
