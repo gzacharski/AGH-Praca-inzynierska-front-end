44769debfd926eaae86f9e4bfa100e35a7a33e1b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 13:22:18 +0100

    'About' comp.: add styles file and add tests.

--- src/main/pages/about/About.js
+++ src/main/pages/about/About.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { useStyles } from "./About.styles";
 
-const useStyles=makeStyles({
-    root : {
-        fontWeight: 'bold',
-        padding: '30px'
-    }
-})
-
-export default function About(){
-    const classes=useStyles();
-    return(
-        <Container maxWidth='xl' component='main' data-testid='main-container'>
-        <Typography variant='h5' className={classes.root} align='center'>
-            O nas
-        </Typography>
-        </Container>
-    );
+export default function About() {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="xl" component="main" data-testid="main-container">
+      <Typography variant="h5" className={classes.root} align="center">
+        O nas
+      </Typography>
+    </Container>
+  );
 }
