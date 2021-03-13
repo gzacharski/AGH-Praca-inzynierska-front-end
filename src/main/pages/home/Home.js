07101f444954e11cecce77198b95ba2ed22ea0c6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 20:46:05 +0100

    'Home' comp.: move style to another file.

--- src/main/pages/home/Home.js
+++ src/main/pages/home/Home.js
@@ -1,40 +1,37 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { addUser } from '../../store/model/action/creators';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { addUser } from "../../store/model/action/creators";
 import Container from "@material-ui/core/Container";
-
-const useStyles = makeStyles({
-    root: {
-        fontWeight: 'bold',
-        padding: '30px'
-    }
-})
+import { useStyles } from "./Home.styles";
 
 const Home = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const userList = (users) => users.map(user => <h4>{user}</h4>);
-    const tempUsers = ["Test user"]
+  const userList = (users) => users.map((user) => <h4>{user}</h4>);
+  const tempUsers = ["Test user"];
 
-    return (
-        <Container maxWidth='xl' component='main' data-testid='main-container'>
-        <Typography variant='h5' className={classes.root} align='center'>
-            Strona główna
-            <br />
-            <Button variant="contained" color="primary" onClick={() => props.addUser(tempUsers[0])}>
-                Test button
-            </Button>
-            {props.users && userList(props.users)}
-        </Typography>
-        </Container>
-    );
-}
+  return (
+    <Container maxWidth="xl" component="main" data-testid="main-container">
+      <Typography variant="h5" className={classes.root} align="center">
+        Strona główna
+        <br />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => props.addUser(tempUsers[0])}
+        >
+          Test button
+        </Button>
+        {props.users && userList(props.users)}
+      </Typography>
+    </Container>
+  );
+};
 
-const mapStateToProps = (store) => ({ users: store.modelData.users })
+const mapStateToProps = (store) => ({ users: store.modelData.users });
 
-const mapDispatchToProps = { addUser }
+const mapDispatchToProps = { addUser };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
