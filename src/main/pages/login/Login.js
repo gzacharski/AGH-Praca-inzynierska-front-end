2cc836d3074f74457462e19ed6a656132fec9faf
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-13 23:33:36 +0100

    Add Login page.

--- src/main/pages/login/Login.js
+++ src/main/pages/login/Login.js
@@ -1,7 +1,15 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import React from "react";
+import { Footer } from "../../layout";
 import { useStyles } from "./Login.styles";
 
 export default function Login() {
@@ -17,18 +25,75 @@ export default function Login() {
         xl={10}
         className={classes.image}
       />
-      <Grid item xs={12} sm={8} md={5} lg={3} xl={2} elevation={6}>
-        <Typography variant="h5" className={classes.heading} align="center">
-          Zaloguj się
-        </Typography>
-        <form className={classes.form}>
-          <TextField
-            id="outlined-email"
-            label="Email"
-            required
-            variant="outlined"
-          />
-        </form>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        lg={3}
+        xl={2}
+        elevation={6}
+        component={Paper}
+        square
+      >
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5" className={classes.heading} align="center">
+            Zaloguj się
+          </Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Hasło"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Zapamiętaj mnie"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Zaloguj
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Nie pamiętasz hasła?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Utwórz konto"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+          <Footer />
+        </div>
       </Grid>
     </Grid>
   );
--- src/main/pages/login/Login.styles.js
+++ src/main/pages/login/Login.styles.js
@@ -1,17 +1,15 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     fontWeight: "bold",
-    padding: "30px",
-    background: "yellow",
+    // padding: "30px",
+    // background: "yellow",
     height: "100vh",
   },
-  form: {},
   heading: {
     fontWeight: "bold",
     padding: "30px",
-    background: "red",
   },
   image: {
     backgroundImage: "url(https://source.unsplash.com/random)",
@@ -19,6 +17,27 @@ const useStyles = makeStyles({
     backgroundPosition: "center",
     backgroundSize: "cover",
   },
-});
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    // minHeight: "90vh",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: '#1e88e5'
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  footer:{
+    marginTop: 'auto'
+  }
+}));
 
 export { useStyles };
