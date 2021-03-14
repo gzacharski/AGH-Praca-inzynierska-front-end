36035c897f36c96b60aadf8bc314bbc5d53c9277
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-14 01:49:38 +0100

    Install react-app-rewired

--- src/main/pages/login/Login.styles.js
+++ src/main/pages/login/Login.styles.js
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: '#1e88e5'
+    backgroundColor: "#1e88e5",
   },
   form: {
     width: "100%",
@@ -34,10 +34,10 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    backgroundColor: '#1e88e5'
+    backgroundColor: "#1e88e5",
   },
-  footer:{
-    marginTop: 'auto'
+  footer: {
+    marginTop: "auto",
   },
 }));
 
