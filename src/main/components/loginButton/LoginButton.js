583308c4a2c10e5ab32601c04e3fd76d6bf4643b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-14 02:03:50 +0100

    Remove unused variable.

--- src/main/components/loginButton/LoginButton.js
+++ src/main/components/loginButton/LoginButton.js
@@ -1,10 +1,8 @@
 import Button from "@material-ui/core/Button";
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { useStyles } from "./LoginButton.styles";
 
 const LoginButton = (props) => {
-  const classes=useStyles();
   const handleClick = (history) => history.push("/login");
   return (
     <Button
