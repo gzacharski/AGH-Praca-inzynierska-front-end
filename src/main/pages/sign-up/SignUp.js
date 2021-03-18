e54a7542ede48da05e7d2ed073379468904d2ce3
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-03-18 23:30:48 +0100

    First draft of sing up form.

--- src/main/pages/sign-up/SignUp.js
+++ src/main/pages/sign-up/SignUp.js
@@ -1,17 +1,134 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { useStyles } from "./SignUp.styles";
+import { useFormik } from 'formik';
+import { useStyles } from './SignUp.styles';
+import "./SignUp.css"
 
-export default function SignUp() {
+const validate = (values) => {
+   const errors = {};
+
+   if (!values.name) {
+      errors.name = 'Imię jest wymagane';
+   } else if (values.name.length < 2) {
+      errors.name = 'Podaj minimalnie dwa znaki';
+   } else if (values.name.length > 20) {
+      errors.name = 'Maksymalna ilość 20 znaków';
+   }
+
+   if (!values.surname) {
+      errors.surname = 'Nazwisko jest wymagane';
+   } else if (values.surname.length < 2) {
+      errors.surname = 'Podaj minimalnie dwa znaki';
+   } else if (values.surname.length > 20) {
+      errors.surname = 'Maksymalna ilość 20 znaków';
+   }
+
+   if (!values.email) {
+      errors.email = 'Email jest wymagany';
+   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Nieprawidłowy adress email';
+   }
+
+   if(!values.password1){
+      errors.password1="Hasło jest wymagane";
+   }else if(values.password1!==values.password2){
+      errors.password1="Niezgodność podanych haseł";
+   }else if(values.password1.length<8){
+      errors.password1="Hasło musi zawierać conajmniej 8 znaków."
+   }else if(values.password2.length>24){
+      errors.password1="Hasło musi zawierać maksymalnie 24 znaki."
+   }
+
+   if(!values.password2){
+      errors.password2="Pole wymagane";
+   }else if(values.password2!==values.password1){
+      errors.password2="Niezgodność podanych haseł";
+   }
+
+   return errors;
+};
+
+const SignUp = () => {
    const classes = useStyles();
+   const formik = useFormik({
+      initialValues: {
+         name: '',
+         surname: '',
+         email: '',
+         password1: '',
+         password2: '',
+      },
+      validate,
+      onSubmit: (values) => {
+         alert(JSON.stringify(values, null, 2));
+      },
+   });
+
    return (
-      <>
-         <TextField
-            id="text-id"
-            label="Filled"
-            className={classes.root}
-         />
-         <div>Sing Up form</div>
-      </>
+      <form onSubmit={formik.handleSubmit} data-testid='sign-up-form'>
+         <div>
+            <label htmlFor="name">Imię</label>
+            <input
+               id="name"
+               name="name"
+               type="text"
+               placeholder="Imię"
+               onChange={formik.handleChange}
+               value={formik.values.name}
+            />
+            {formik.errors.name && <div className="warning">{formik.errors.name}</div>}
+         </div>
+         <div>
+            <label htmlFor="surname">Nazwisko</label>
+            <input
+               id="surname"
+               name="surname"
+               type="text"
+               placeholder="Nazwisko"
+               onChange={formik.handleChange}
+               value={formik.values.surname}
+            />
+            {formik.errors.surname && <div className="warning">{formik.errors.surname}</div>}
+         </div>
+         <div>
+            <label htmlFor="email">Email</label>
+            <input
+               id="email"
+               name="email"
+               type="email"
+               placeholder="Email"
+               onChange={formik.handleChange}
+               value={formik.values.email}
+            />
+            {formik.errors.email && <div className="warning">{formik.errors.email}</div>}
+         </div>
+         <div>
+            <label htmlFor="password1">Hasło</label>
+            <input
+               id="password1"
+               name="password1"
+               type="password"
+               placeholder="Hasło"
+               onChange={formik.handleChange}
+               value={formik.values.password1}
+            />
+            {formik.errors.password1 && <div className="warning">{formik.errors.password1}</div>}
+         </div>
+         <div>
+            <label htmlFor="password2">Powtórz hasło</label>
+            <input
+               id="password2"
+               name="password2"
+               type="password"
+               placeholder="Powtórz hasło"
+               onChange={formik.handleChange}
+               value={formik.values.password2}
+            />
+            {formik.errors.password2 && <div className="warning">{formik.errors.password2}</div>}
+         </div>
+         <button type="submit">Zarejestruj się</button>
+      </form>
    );
-}
+};
+
+export default SignUp;
--- src/main/pages/sign-up/SignUp.test.js
+++ src/main/pages/sign-up/SignUp.test.js
@@ -1,40 +1,152 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
 import { render, screen } from 'src/testUtils';
+import userEvent from '@testing-library/user-event';
 import SignUp from './SignUp';
 
-const mockStore = configureStore([]);
+beforeEach(() => {
+   render(<SignUp />);
+});
 
-describe('SignUp component should render in DOM', () => {
-   let container;
-   let store;
+describe('SignUp component', () => {
+   test('should render.', () => {
+      expect(screen.getByTestId('sign-up-form')).toBeInTheDocument();
+   });
+});
 
-   beforeEach(() => {
-      container = document.createElement('div');
-      store = mockStore({
-         modelData: {
-            users: [],
-         },
-         stateData: {
-            menuIsOpen: false,
-         },
-      });
+describe('SignUp form should have:', () => {
+   test('name field', () => {
+      expect(screen.getByPlaceholderText('Imię')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Imię')).toBeVisible();
    });
 
-   afterEach(() => {
-      ReactDOM.unmountComponentAtNode(container);
-      container.remove();
-      container = null;
+   test('surname field', () => {
+      expect(screen.getByPlaceholderText('Nazwisko')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Nazwisko')).toBeVisible();
    });
 
-   test('should render', () => {
-      render(
-         <Provider store={store}>
-            <SignUp />
-         </Provider>,
+   test('email field', () => {
+      expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Email')).toBeVisible();
+   });
+
+   test('password field', () => {
+      expect(screen.getByPlaceholderText('Hasło')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Hasło')).toBeVisible();
+   });
+
+   test('repeated password field', () => {
+      expect(screen.getByPlaceholderText('Powtórz hasło')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Powtórz hasło')).toBeVisible();
+   });
+
+   test('submit button', () => {
+      const button = screen.getByRole('button');
+      expect(button).toBeInTheDocument();
+      expect(button).toBeVisible();
+      expect(button.firstChild.textContent).toEqual('Zarejestruj się');
+   });
+});
+
+describe('Field name:', () => {
+   const timeToWriteNextCharacterInMS = 50;
+
+   test('should properly change value.', async () => {
+      expect(screen.getByPlaceholderText('Imię')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Imię'),
+         'Janek',
+         timeToWriteNextCharacterInMS,
       );
-      expect(screen.queryByText('Sing Up form')).toBeInTheDocument();
+      expect(await screen.findByPlaceholderText('Imię')).toHaveValue('Janek');
+   });
+
+   test('should show warning that field is required', async () => {
+      expect(screen.getByPlaceholderText('Imię')).toHaveValue('');
+      const button = screen.getByRole('button');
+      userEvent.click(button);
+      expect(await screen.findByPlaceholderText('Imię')).toHaveValue('');
+      expect(await screen.findByText('Imię jest wymagane')).toBeInTheDocument();
+   });
+
+   test('should show warning if not enough characters.', async () => {
+      expect(screen.getByPlaceholderText('Imię')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Imię'),
+         'X',
+         timeToWriteNextCharacterInMS,
+      );
+      expect(await screen.findByPlaceholderText('Imię')).toHaveValue('X');
+      expect(
+         await screen.findByText('Podaj minimalnie dwa znaki'),
+      ).toBeInTheDocument();
+   });
+
+   test('should show warning if too many characters', async () => {
+      expect(screen.getByPlaceholderText('Imię')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Imię'),
+         'Xfdgfsgashtjryerhyuhjrtjretyerthfgsa',
+         timeToWriteNextCharacterInMS,
+      );
+      expect(await screen.findByPlaceholderText('Imię')).toHaveValue(
+         'Xfdgfsgashtjryerhyuhjrtjretyerthfgsa',
+      );
+      expect(
+         await screen.findByText('Maksymalna ilość 20 znaków'),
+      ).toBeInTheDocument();
+   });
+});
+
+describe('Surname field:', () => {
+   const timeToWriteNextCharacterInMS = 50;
+
+   test('should properly change value.', async () => {
+      expect(screen.getByPlaceholderText('Nazwisko')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Nazwisko'),
+         'Janek',
+         timeToWriteNextCharacterInMS,
+      );
+      expect(await screen.findByPlaceholderText('Nazwisko')).toHaveValue(
+         'Janek',
+      );
+   });
+
+   test('should show warning that field is required', async () => {
+      expect(screen.getByPlaceholderText('Nazwisko')).toHaveValue('');
+      const button = screen.getByRole('button');
+      userEvent.click(button);
+      expect(await screen.findByPlaceholderText('Nazwisko')).toHaveValue('');
+      expect(
+         await screen.findByText('Nazwisko jest wymagane'),
+      ).toBeInTheDocument();
+   });
+
+   test('should show warning if not enough characters.', async () => {
+      expect(screen.getByPlaceholderText('Nazwisko')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Nazwisko'),
+         'X',
+         timeToWriteNextCharacterInMS,
+      );
+      expect(await screen.findByPlaceholderText('Nazwisko')).toHaveValue('X');
+      expect(
+         await screen.findByText('Podaj minimalnie dwa znaki'),
+      ).toBeInTheDocument();
+   });
+
+   test('should show warning if too many characters', async () => {
+      expect(screen.getByPlaceholderText('Nazwisko')).toHaveValue('');
+      userEvent.type(
+         screen.getByPlaceholderText('Nazwisko'),
+         'Xfdgfsgashtjryerhyuhjrtjretyerthfgsa',
+         timeToWriteNextCharacterInMS,
+      );
+      expect(await screen.findByPlaceholderText('Nazwisko')).toHaveValue(
+         'Xfdgfsgashtjryerhyuhjrtjretyerthfgsa',
+      );
+      expect(
+         await screen.findByText('Maksymalna ilość 20 znaków'),
+      ).toBeInTheDocument();
    });
 });
