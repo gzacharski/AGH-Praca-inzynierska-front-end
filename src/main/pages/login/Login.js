c0adb1e6677279864cd702d25817cd51cb3d3c0c
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-19 08:25:16 +0200

    Add Slide fro MUI. Organize imports

--- src/main/pages/login/Login.js
+++ src/main/pages/login/Login.js
@@ -1,13 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
+import {
+   Avatar,
+   Button,
+   Checkbox,
+   FormControlLabel,
+   Grid,
+   Link,
+   Paper,
+   Slide,
+   TextField,
+   Typography,
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import React from 'react';
 import { Footer } from '../../layout';
@@ -16,93 +19,95 @@ import { useStyles } from './Login.styles';
 export default function Login() {
    const classes = useStyles();
    return (
-      <Grid container className={classes.root}>
-         <Grid
-            item
-            xs={false}
-            sm={4}
-            md={7}
-            lg={9}
-            xl={10}
-            className={classes.image}
-         />
-         <Grid
-            item
-            xs={12}
-            sm={8}
-            md={5}
-            lg={3}
-            xl={2}
-            elevation={6}
-            component={Paper}
-            square
-         >
-            <div className={classes.paper}>
-               <Avatar className={classes.avatar}>
-                  <LockOutlinedIcon />
-               </Avatar>
-               <Typography
-                  variant="h5"
-                  className={classes.heading}
-                  align="center"
-               >
-                  Zaloguj się
-               </Typography>
-               <form className={classes.form} noValidate>
-                  <TextField
-                     variant="outlined"
-                     margin="normal"
-                     required
-                     fullWidth
-                     id="email"
-                     label="Email"
-                     name="email"
-                     autoComplete="email"
-                     autoFocus
-                  />
-                  <TextField
-                     variant="outlined"
-                     margin="normal"
-                     required
-                     fullWidth
-                     name="password"
-                     label="Hasło"
-                     type="password"
-                     id="password"
-                     autoComplete="current-password"
-                  />
-                  <FormControlLabel
-                     control={<Checkbox value="remember" color="primary" />}
-                     label="Zapamiętaj mnie"
-                  />
-                  <Button
-                     type="submit"
-                     fullWidth
-                     variant="contained"
-                     color="primary"
-                     className={classes.submit}
+      <Slide direction="right" in mountOnEnter unmountOnExit timeout={400}>
+         <Grid container className={classes.root}>
+            <Grid
+               item
+               xs={false}
+               sm={4}
+               md={7}
+               lg={9}
+               xl={10}
+               className={classes.image}
+            />
+            <Grid
+               item
+               xs={12}
+               sm={8}
+               md={5}
+               lg={3}
+               xl={2}
+               elevation={6}
+               component={Paper}
+               square
+            >
+               <div className={classes.paper}>
+                  <Avatar className={classes.avatar}>
+                     <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography
+                     variant="h5"
+                     className={classes.heading}
+                     align="center"
                   >
-                     Zaloguj
-                  </Button>
-               </form>
-               <Grid container>
-                  <Grid item xs>
-                     <Link href="#" variant="body2">
-                        Nie pamiętasz hasła?
-                     </Link>
-                  </Grid>
-                  <Grid item>
-                     <Link
-                        href={`${process.env.PUBLIC_URL}/sign-up`}
-                        variant="body2"
+                     Zaloguj się
+                  </Typography>
+                  <form className={classes.form} noValidate>
+                     <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                     />
+                     <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Hasło"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                     />
+                     <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Zapamiętaj mnie"
+                     />
+                     <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
                      >
-                        Utwórz konto
-                     </Link>
+                        Zaloguj
+                     </Button>
+                  </form>
+                  <Grid container>
+                     <Grid item xs>
+                        <Link href="#" variant="body2">
+                           Nie pamiętasz hasła?
+                        </Link>
+                     </Grid>
+                     <Grid item>
+                        <Link
+                           href={`${process.env.PUBLIC_URL}/sign-up`}
+                           variant="body2"
+                        >
+                           Utwórz konto
+                        </Link>
+                     </Grid>
                   </Grid>
-               </Grid>
-               <Footer />
-            </div>
+                  <Footer />
+               </div>
+            </Grid>
          </Grid>
-      </Grid>
+      </Slide>
    );
 }
--- src/main/pages/sign-up/SignUp.js
+++ src/main/pages/sign-up/SignUp.js
@@ -1,12 +1,13 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import Avatar from '@material-ui/core/Avatar';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Container from '@material-ui/core/Container';
+import {
+   Avatar,
+   Backdrop,
+   CircularProgress,
+   Container,
+   Snackbar,
+   Slide,
+   Typography,
+} from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
-import Snackbar from '@material-ui/core/Snackbar';
-import Typography from '@material-ui/core/Typography';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import React, { useState } from 'react';
 import { SingUpForm } from 'src/main/components/forms';
@@ -27,45 +28,53 @@ export default function SingUp() {
    };
 
    return (
-      <Container maxWidth="sm" component="main" data-testid="main-container">
-         <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-               <LockOutlinedIcon />
-            </Avatar>
-            <Typography variant="h5" className={classes.heading} align="center">
-               Zarejestruj się
-            </Typography>
-            <SingUpForm
-               setSuccess={setSuccess}
-               setDisplayBackdrop={setDisplayBackdrop}
-               setDisplaySnackBar={setDisplaySnackBar}
-               setResponseMessage={setResponseMessage}
-               setError={setError}
-            />
-            <Footer />
-            <Backdrop className={classes.backdrop} open={displayBackDrop}>
-               <CircularProgress
-                  color="inherit"
-                  data-testid="sign-up-backdrop"
-               />
-            </Backdrop>
-            <Snackbar
-               open={displaySnackBar}
-               autoHideDuration={3000}
-               onClose={handleCloseSnackBar}
-            >
-               <MuiAlert
-                  elevation={6}
-                  variant="filled"
-                  onClose={handleCloseSnackBar}
-                  // eslint-disable-next-line no-nested-ternary
-                  severity={error ? 'error' : ( success ? 'success' : 'warning')}
-                  data-testid="sign-up-snackbar"
+      <Slide direction="right" in mountOnEnter unmountOnExit timeout={400}>
+         <Container maxWidth="sm" component="main" data-testid="main-container">
+            <div className={classes.paper}>
+               <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+               </Avatar>
+               <Typography
+                  variant="h5"
+                  className={classes.heading}
+                  align="center"
                >
-                  {responseMessage}
-               </MuiAlert>
-            </Snackbar>
-         </div>
-      </Container>
+                  Zarejestruj się
+               </Typography>
+               <SingUpForm
+                  setSuccess={setSuccess}
+                  setDisplayBackdrop={setDisplayBackdrop}
+                  setDisplaySnackBar={setDisplaySnackBar}
+                  setResponseMessage={setResponseMessage}
+                  setError={setError}
+               />
+               <Footer />
+               <Backdrop className={classes.backdrop} open={displayBackDrop}>
+                  <CircularProgress
+                     color="inherit"
+                     data-testid="sign-up-backdrop"
+                  />
+               </Backdrop>
+               <Snackbar
+                  open={displaySnackBar}
+                  autoHideDuration={3000}
+                  onClose={handleCloseSnackBar}
+               >
+                  <MuiAlert
+                     elevation={6}
+                     variant="filled"
+                     onClose={handleCloseSnackBar}
+                     severity={
+                        // eslint-disable-next-line no-nested-ternary
+                        error ? 'error' : success ? 'success' : 'warning'
+                     }
+                     data-testid="sign-up-snackbar"
+                  >
+                     {responseMessage}
+                  </MuiAlert>
+               </Snackbar>
+            </div>
+         </Container>
+      </Slide>
    );
 }
