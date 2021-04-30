05e989f3fadd4b3d8d907185a6c550587047ea8a
bartoszkordek <bartosz.kordek10@gmail.com>
2021-04-30 02:50:41 +0200

    send status request

--- src/main/components/forms/trainings-show/ShowTrainings.js
+++ src/main/components/forms/trainings-show/ShowTrainings.js
@@ -1,12 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import axios from 'axios';
+import { trainingsServiceURL } from 'src/main/data/urls';
 import { useStyles } from './ShowTrainingsForm.styles';
 
 
+
+
+
+
+
 const ShowTrainings = () => {
+
+    const sendRequest  = () => {
+        axios
+            .get(`http://localhost:8020/trainings/status`, {
+                validateStatus: (status) =>
+                (status >= 200 && status < 300) || status === 409,
+            })
+            .then((response) => {
+                console.log("response: ", response.data);
+            })
+    }
+
+
+
     const classes = useStyles();
     return (
         <form>
@@ -20,8 +41,10 @@ const ShowTrainings = () => {
             >
                 Offers
             </Button>
+            
         </form>
     )
     
     };
+
 export default ShowTrainings;
