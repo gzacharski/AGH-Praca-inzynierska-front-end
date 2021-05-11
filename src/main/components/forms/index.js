8e4710222fe0822acdae14d6f38ecc6337d8fcf5
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-12 01:52:58 +0200

    Add ResetPasswordForm to index

--- src/main/components/forms/index.js
+++ src/main/components/forms/index.js
@@ -1,5 +1,6 @@
 import SingUpForm from './sign-up/SignUp';
 import LogInForm from './log-in/LogInForm';
 import ConfirmResetPasswordForm from './confirmResetPassword/ConfirmResetPasswordForm';
+import ResetPasswordForm from './resetPassword/ResetPasswordForm';
 
-export { SingUpForm, LogInForm, ConfirmResetPasswordForm };
+export { SingUpForm, LogInForm, ConfirmResetPasswordForm, ResetPasswordForm };
