2acadb4c305e2fb6aaa289d82a4e5c0af9e0d2d7
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-11 23:02:59 +0200

    Refactor ConfirmRegistrationPage

--- src/main/pages/confirmRegistration/ConfirmRegistrationPage.js
+++ src/main/pages/confirmRegistration/ConfirmRegistrationPage.js
@@ -1,24 +1,10 @@
 import React, { useState } from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
-import {
-   Avatar,
-   Container,
-   CircularProgress,
-   Typography,
-} from '@material-ui/core';
-import {
-   LockOutlined,
-   CheckCircle,
-   Error,
-   ReportProblem,
-} from '@material-ui/icons';
-import {
-   ActivateAccountButton,
-   ActivateAccountRefreshButton,
-   LoginButton,
-} from 'src/main/components/buttons';
+import { Container, CircularProgress, Typography } from '@material-ui/core';
 import { Footer } from 'src/main/layout';
+import { ConfirmationIcon } from 'src/main/components/icons';
 import { useStyles } from './ConfirmRegistrationPage.styles';
+import ConfirmationButton from './confirmationButton/ConfirmationButton';
 
 function useQuery() {
    return new URLSearchParams(useLocation().search);
@@ -32,26 +18,6 @@ export default function ConfirmRegistration() {
    const [message, setMessage] = useState(null);
    const [onRequest, setOnRequest] = useState(false);
 
-   const renderProperButton = () => {
-      if (status === 200) return <LoginButton />;
-      if (status === 401 || status === 404)
-         return (
-            <ActivateAccountRefreshButton
-               setMessage={setMessage}
-               setOnRequest={setOnRequest}
-               setStatus={setStatus}
-            />
-         );
-      if (status === 500) return null;
-      return (
-         <ActivateAccountButton
-            setMessage={setMessage}
-            setOnRequest={setOnRequest}
-            setStatus={setStatus}
-         />
-      );
-   };
-
    const renderMessage = () =>
       message && <div className={classes.message}>{message}</div>;
 
@@ -61,39 +27,13 @@ export default function ConfirmRegistration() {
       return (
          <>
             {renderMessage()}
-            {renderProperButton()}
+            <ConfirmationButton
+               status={status}
+               setMessage={setMessage}
+               setOnRequest={setOnRequest}
+               setStatus={setStatus}
+            />
          </>
-      );
-   };
-
-   const handleIconChange = () => {
-      if (!onRequest) {
-         if (status === 200) {
-            return (
-               <Avatar className={classes.avatar200}>
-                  <CheckCircle data-testid="check-circle" />
-               </Avatar>
-            );
-         }
-         if (status === 401 || status === 404) {
-            return (
-               <Avatar className={classes.avatar400}>
-                  <ReportProblem data-testid="report-problem" />
-               </Avatar>
-            );
-         }
-         if (status === 500) {
-            return (
-               <Avatar className={classes.avatar500}>
-                  <Error data-testid="error" />
-               </Avatar>
-            );
-         }
-      }
-      return (
-         <Avatar className={classes.avatar}>
-            <LockOutlined />
-         </Avatar>
       );
    };
 
@@ -102,7 +42,7 @@ export default function ConfirmRegistration() {
          {!hasToken && <Redirect to="/" />}
          <Container maxWidth="sm" component="main" className={classes.root}>
             <div className={classes.paper}>
-               {handleIconChange()}
+               <ConfirmationIcon onRequest={onRequest} status={status} />
                <Typography
                   variant="h5"
                   className={classes.heading}
--- src/main/pages/confirmRegistration/ConfirmRegistrationPage.styles.js
+++ src/main/pages/confirmRegistration/ConfirmRegistrationPage.styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
    root: {
       fontWeight: 'bold',
       height: '100vh',
@@ -15,33 +15,17 @@ const useStyles = makeStyles((theme) => ({
       left: '50%',
       transform: 'translate(-50%, -50%)',
    },
-   avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: '#1e88e5',
-   },
-   avatar200:{
-      margin: theme.spacing(1),
-      backgroundColor: '#8bc34a',
-   },
-   avatar400:{
-      margin: theme.spacing(1),
-      backgroundColor: '#ffc107',
-   },
-   avatar500:{
-      margin: theme.spacing(1),
-      backgroundColor: '#ab003c',
-   },
    activatButton: {
       margin: '30px',
    },
    message: {
       margin: '30px',
-      textAlign: 'center'
+      textAlign: 'center',
    },
    heading: {
       fontWeight: 'bold',
       margin: '30px',
    },
-}));
+});
 
 export { useStyles };
