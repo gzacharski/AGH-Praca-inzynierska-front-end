faf0e2025c04a666763b193a0d0b6a4930b240be
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-12 01:51:39 +0200

    Add ResetPasswordForm and its tests

---
+++ src/main/components/forms/resetPassword/ResetPasswordForm.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Button, Grid, TextField } from '@material-ui/core';
+import { userServiceURL } from 'src/main/data/urls';
+import { NETWORK_ERROR } from 'src/main/data/messages';
+import { useStyles } from './ResetPasswordForm.styles';
+
+const isNotEmpty = (text) => text && text.length !== 0;
+
+const validationSchema = Yup.object({
+   email: Yup.string()
+      .email('Nieprawidłowy adress email')
+      .required('Email jest wymagany'),
+});
+
+const ConfirmResetPasswordForm = (props) => {
+   const { setMessage, setOnRequest, setStatus } = props;
+
+   const classes = useStyles();
+
+   const formik = useFormik({
+      initialValues: {
+         email: '',
+      },
+      validationSchema,
+      onSubmit: (values) => {
+         setOnRequest(true);
+         axios
+            .post(
+               `${userServiceURL}/resetPassword`,
+               {
+                  email: values.email,
+               },
+               {
+                  headers: {
+                     'Accept-Language': 'pl',
+                  },
+               },
+            )
+            .then((response) => {
+               setStatus(response.status);
+               setMessage(response.data.message);
+            })
+            .catch((error) => {
+               if (error.response === undefined) {
+                  setStatus(500);
+                  setMessage(NETWORK_ERROR);
+               } else {
+                  setStatus(error.response?.status);
+                  setMessage(error.response?.data?.message);
+               }
+            })
+            .finally(() => {
+               // timeout for tests
+               setTimeout(() => setOnRequest(false), 700);
+            });
+      },
+   });
+
+   return (
+      <form
+         onSubmit={formik.handleSubmit}
+         data-testid="reset-password-form"
+         className={classes.form}
+         noValidate
+      >
+         <Grid container spacing={2}>
+            <Grid item xs={12}>
+               <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  error={
+                     formik.touched.email && isNotEmpty(formik.errors.email)
+                  }
+                  helperText={formik.touched.email && formik.errors.email}
+               />
+            </Grid>
+         </Grid>
+         <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+         >
+            Wyślij link do zmiany hasła
+         </Button>
+      </form>
+   );
+};
+
+export default ConfirmResetPasswordForm;
---
+++ src/main/components/forms/resetPassword/ResetPasswordForm.test.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import axios from 'axios';
+import { render, screen } from 'src/testUtils';
+import { act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { userServiceURL } from 'src/main/data/urls';
+import ResetPasswordForm from './ResetPasswordForm';
+
+jest.mock('axios');
+
+describe('ResetPasswordForm', () => {
+   beforeEach(() => {
+      const setMessage = jest.fn();
+      const setOnRequest = jest.fn();
+      const setStatus = jest.fn();
+
+      render(
+         <ResetPasswordForm
+            setMessage={setMessage}
+            ssetOnRequest={setOnRequest}
+            setStatus={setStatus}
+         />,
+      );
+   });
+
+   test('ResetPasswordForm component should render', () => {
+      expect(screen.getByTestId('reset-password-form')).toBeInTheDocument();
+   });
+
+   describe('ResetPassword form should have:', () => {
+      test('password field', () => {
+         expect(screen.getByLabelText(/Email/)).toBeInTheDocument();
+         expect(screen.getByText('Email')).toBeVisible();
+      });
+
+      test('submit button', () => {
+         const button = screen.getByRole('button');
+         expect(button).toBeInTheDocument();
+         expect(button).toBeVisible();
+         expect(button).toHaveTextContent('Wyślij link do zmiany hasła');
+      });
+   });
+
+   describe('Email field:', () => {
+      const timeToWriteNextCharacterInMS = 50;
+
+      test.each([
+         ['test@test.com'],
+         ['T@t.com'],
+         ['jan.kowalski.28@test.test.com'],
+      ])('email should properly change value.', async (testEmail) => {
+         expect(screen.getByLabelText(/Email/)).toHaveValue('');
+         userEvent.type(
+            screen.getByLabelText(/Email/),
+            testEmail,
+            timeToWriteNextCharacterInMS,
+         );
+         expect(await screen.findByLabelText(/Email/)).toHaveValue(testEmail);
+      });
+
+      test('should display warning that field is required', async () => {
+         expect(screen.getByLabelText(/Email/)).toHaveValue('');
+         const button = screen.getByRole('button');
+         userEvent.click(button);
+         expect(await screen.findByLabelText(/Email/)).toHaveValue('');
+         expect(
+            await screen.findByText(/Email jest wymagany/),
+         ).toBeInTheDocument();
+      });
+
+      test.each([
+         ['Test@test'],
+         ['testtest.com'],
+         // ['test @test.com'], // not tested - bug in library
+         // ['test@ test.com'],  // not tested - bug in library
+         ['test.com'],
+         ['test@@test.com'],
+         ['test'],
+      ])(
+         'should display warning if invalid email %s address is provided',
+         async (testEmail) => {
+            expect(screen.getByLabelText(/Email/)).toHaveValue('');
+            userEvent.type(
+               screen.getByLabelText(/Email/),
+               testEmail,
+               timeToWriteNextCharacterInMS,
+            );
+            userEvent.click(screen.getByTestId('reset-password-form'));
+            expect(
+               await screen.findByText('Nieprawidłowy adress email'),
+            ).toBeInTheDocument();
+         },
+      );
+   });
+
+   describe('When button clicked', () => {
+      test('should send post request', async () => {
+         const confirmationURL = `${userServiceURL}/resetPassword`;
+         axios.post.mockImplementationOnce(() =>
+            Promise.resolve({ status: 200, data: {} }),
+         );
+
+         act(() => {
+            userEvent.click(screen.getByRole('button'));
+         });
+
+         Promise.all([
+            waitFor(() => expect(axios.post).toBeCalledTimes(1)),
+            waitFor(() =>
+               expect(axios.post).toHaveBeenCalledWith(confirmationURL, {
+                  headers: {
+                     'Accept-Language': 'pl',
+                  },
+               }),
+            ),
+         ]);
+      });
+   });
+});
