6754020c1f3d7b8e8936345acd001c7c4499ee6a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-11 23:21:12 +0200

    Add ConfirmationResetPasswordContent component and its tests.

---
+++ src/main/pages/confirmResetPassword/confirmationContent/ConfirmationResetPasswordContent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+   ActivateAccountRefreshButton,
+   LoginButton,
+} from 'src/main/components/buttons';
+import { ResetPasswordForm } from 'src/main/components/forms';
+
+const ConfirmationResetPasswordContent = (props) => {
+   const { status, setMessage, setOnRequest, setStatus } = props;
+
+   if (status === 200) return <LoginButton />;
+   if (status === 401 || status === 404)
+      return (
+         <ActivateAccountRefreshButton
+            setMessage={setMessage}
+            setOnRequest={setOnRequest}
+            setStatus={setStatus}
+         />
+      );
+   if (status === 500) return null;
+   return (
+      <ResetPasswordForm
+         setMessage={setMessage}
+         setOnRequest={setOnRequest}
+         setStatus={setStatus}
+      />
+   );
+};
+
+export default ConfirmationResetPasswordContent;
