488d0c249ae4f0cf8b148347e8b67897114f9d6f
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-22 17:54:46 +0200

    Add SettingsPage

---
+++ src/main/pages/account/settings/SettingsPage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from 'src/testUtils';
+import SettingsPage from './SettingsPage';
+
+describe('SettingsPage', () => {
+   beforeEach(() => {
+      render(<SettingsPage />);
+   });
+
+   test('should contain ', () => {
+      expect(screen.getByText('Ustawienia')).toBeInTheDocument();
+   });
+});
