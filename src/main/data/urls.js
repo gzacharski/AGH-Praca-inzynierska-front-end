374fb7a227d4dd518ea90bdc9799363aa12e99b1
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-25 16:12:43 +0200

    Add new url

--- src/main/data/urls.js
+++ src/main/data/urls.js
@@ -4,8 +4,12 @@ const port = 8020;
 const accountService = 'account';
 const authService = 'auth';
 const trainingsService = 'trainings';
+const offerService = 'offer';
 
 export const accountServiceURL = `${protocol}://${hostname}:${port}/${accountService}`;
 export const authServiceURL = `${protocol}://${hostname}:${port}/${authService}`;
 export const trainingsServiceURL = `${protocol}://${hostname}:${port}/${trainingsService}`;
 export const apiGateway = `${protocol}://${hostname}:${port}`;
+
+//TODO to fix when back-end is ready
+export const offerServiceURL = `${protocol}://${hostname}:${port}/${offerService}`;
