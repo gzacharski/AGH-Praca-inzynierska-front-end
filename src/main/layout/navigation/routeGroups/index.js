2878184ceaebb40a1bdeb7e7108addfb065c4a42
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-29 01:28:48 +0200

    Rename AccountRouteGroup

--- src/main/layout/navigation/routeGroups/index.js
+++ src/main/layout/navigation/routeGroups/index.js
@@ -1,4 +1,4 @@
-import AccountRouteGroup from './AAccountRouteGroup';
+import AccountRouteGroup from './AccountRouteGroup';
 import AdminRouteGroup from './AdminRouteGroups';
 import EmployeeRouteGroup from './EmployeeRouteGroups';
 import ManagerRouteGroup from './ManagerRouteGroups';
