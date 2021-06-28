8c5a83c0db12d42dff1acc899149fac6784029ee
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-28 23:06:40 +0200

    Refactor AuthFilter

--- src/main/auth/AuthFilter.js
+++ src/main/auth/AuthFilter.js
@@ -20,25 +20,23 @@ export const withAuthFilter = (Component) => {
    );
 };
 
-const hasRole = (role) => {
+const withHasRole = (Component, role) => {
    const authContext = useContext(AuthContext);
    const { userInfo } = authContext.authState;
-   return userInfo.roles.includes(role);
+   const hasRole = userInfo.roles.includes(role);
+
+   return hasRole && <Component />;
 };
 
-export const hasAdminRole = () => hasRole(ROLE_ADMIN);
-export const hasEmployeeRole = () => hasRole(ROLE_EMPLOYEE);
-export const hasManagerRole = () => hasRole(ROLE_MANAGER);
-export const hasTrainerRole = () => hasRole(ROLE_TRAINER);
-export const hasUserRole = () => hasRole(ROLE_USER);
-
-export const withAdminRole = (Component) => hasAdminRole() && <Component />;
+export const withAdminRole = (Component) => withHasRole(Component, ROLE_ADMIN);
 
 export const withEmployeeRole = (Component) =>
-   hasEmployeeRole() && <Component />;
+   withHasRole(Component, ROLE_EMPLOYEE);
 
-export const withManagerRole = (Component) => hasManagerRole() && <Component />;
+export const withManagerRole = (Component) =>
+   withHasRole(Component, ROLE_MANAGER);
 
-export const withTrainerRole = (Component) => hasTrainerRole() && <Component />;
+export const withTrainerRole = (Component) =>
+   withHasRole(Component, ROLE_TRAINER);
 
-export const withUserRole = (Component) => hasUserRole() && <Component />;
+export const withUserRole = (Component) => withHasRole(Component, ROLE_USER);
