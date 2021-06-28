966a59e3d55fb96c4d0b8260f209d1b99229ea04
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-28 14:51:45 +0200

    Add withHasRole methods and theirs tests.

--- src/main/auth/AuthFilter.js
+++ src/main/auth/AuthFilter.js
@@ -1,6 +1,13 @@
 import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import { AuthContext } from 'src/main/auth';
+import {
+   ROLE_ADMIN,
+   ROLE_EMPLOYEE,
+   ROLE_MANAGER,
+   ROLE_TRAINER,
+   ROLE_USER,
+} from 'src/main/data/roles';
 
 export const withAuthFilter = (Component) => {
    const authContext = useContext(AuthContext);
@@ -12,3 +19,24 @@ export const withAuthFilter = (Component) => {
       </>
    );
 };
+
+const withHasRole = (Component, role) => {
+   const authContext = useContext(AuthContext);
+   const { userInfo } = authContext.authState;
+   const hasRole = userInfo.roles.includes(role);
+
+   return hasRole && <Component />;
+};
+
+export const withAdminRole = (Component) => withHasRole(Component, ROLE_ADMIN);
+
+export const withEmployeeRole = (Component) =>
+   withHasRole(Component, ROLE_EMPLOYEE);
+
+export const withManagerRole = (Component) =>
+   withHasRole(Component, ROLE_MANAGER);
+
+export const withTrainerRole = (Component) =>
+   withHasRole(Component, ROLE_TRAINER);
+
+export const withUserRole = (Component) => withHasRole(Component, ROLE_USER);
--- src/main/auth/AuthFilter.test.js
+++ src/main/auth/AuthFilter.test.js
@@ -1,41 +1,175 @@
 import React from 'react';
 import { MemoryRouter, Route, Switch } from 'react-router-dom';
 import { render, screen } from 'src/testUtils';
+import {
+   ROLE_ADMIN,
+   ROLE_EMPLOYEE,
+   ROLE_MANAGER,
+   ROLE_TRAINER,
+   ROLE_USER,
+} from 'src/main/data/roles';
 import { AuthContext } from './AuthContext';
-import { withAuthFilter } from './AuthFilter';
+import {
+   withAuthFilter,
+   withAdminRole,
+   withEmployeeRole,
+   withManagerRole,
+   withTrainerRole,
+   withUserRole,
+} from './AuthFilter';
 
 const TestComponent = () => withAuthFilter(() => <div>Test component</div>);
+const TestComponentWithAdmin = () =>
+   withAdminRole(() => <div>Test component with admin role</div>);
+
+const TestComponentWithEmployee = () =>
+   withEmployeeRole(() => <div>Test component with employee role</div>);
+
+const TestComponentWithManager = () =>
+   withManagerRole(() => <div>Test component with manager role</div>);
+
+const TestComponentWithTrainer = () =>
+   withTrainerRole(() => <div>Test component with trainer role</div>);
+
+const TestComponentWithUser = () =>
+   withUserRole(() => <div>Test component with user role</div>);
 
 describe('Auth filter', () => {
-   test('should render text', () => {
-      render(
-         <MemoryRouter>
-            <AuthContext.Provider value={{ authState: { token: 'testToken' } }}>
-               <TestComponent />
-            </AuthContext.Provider>
-         </MemoryRouter>,
-      );
-      expect(screen.getByText(/Test component/)).toBeInTheDocument();
+   describe('withAuthFilter', () => {
+      test('should render text', () => {
+         render(
+            <MemoryRouter>
+               <AuthContext.Provider
+                  value={{ authState: { token: 'testToken' } }}
+               >
+                  <TestComponent />
+               </AuthContext.Provider>
+            </MemoryRouter>,
+         );
+         expect(screen.getByText('Test component')).toBeInTheDocument();
+      });
+
+      test('should NOT render text', () => {
+         render(
+            <MemoryRouter initialEntries={['/test']}>
+               <Switch>
+                  <Route
+                     path="/test"
+                     component={() => (
+                        <AuthContext.Provider
+                           value={{ authState: { token: null } }}
+                        >
+                           <TestComponent />
+                        </AuthContext.Provider>
+                     )}
+                  />
+                  <Route
+                     path="/login"
+                     component={() => <div>Login page</div>}
+                  />
+               </Switch>
+            </MemoryRouter>,
+         );
+         expect(screen.queryByText('Test component')).not.toBeInTheDocument();
+      });
    });
 
-   test('should NOT render text', () => {
-      render(
-         <MemoryRouter initialEntries={['/test']}>
-            <Switch>
-               <Route
-                  path="/test"
-                  component={() => (
-                     <AuthContext.Provider
-                        value={{ authState: { token: null } }}
-                     >
-                        <TestComponent />
-                     </AuthContext.Provider>
-                  )}
-               />
-               <Route path="/login" component={() => <div>Login page</div>} />
-            </Switch>
-         </MemoryRouter>,
+   describe('with specific role', () => {
+      test.each([
+         [
+            'withAdminRole',
+            'Test component with admin role',
+            TestComponentWithAdmin,
+            ROLE_ADMIN,
+         ],
+         [
+            'withEmployeeRole',
+            'Test component with employee role',
+            TestComponentWithEmployee,
+            ROLE_EMPLOYEE,
+         ],
+         [
+            'withManagerRole',
+            'Test component with manager role',
+            TestComponentWithManager,
+            ROLE_MANAGER,
+         ],
+         [
+            'withTrainerRole',
+            'Test component with trainer role',
+            TestComponentWithTrainer,
+            ROLE_TRAINER,
+         ],
+         [
+            'withUserRole',
+            'Test component with user role',
+            TestComponentWithUser,
+            ROLE_USER,
+         ],
+      ])(
+         '%s should render text: %s',
+         (name, expectedText, TestedComponent, TestedRole) => {
+            render(
+               <AuthContext.Provider
+                  value={{
+                     authState: {
+                        userInfo: {
+                           roles: [TestedRole],
+                        },
+                     },
+                  }}
+               >
+                  <TestedComponent />
+               </AuthContext.Provider>,
+            );
+            expect(screen.getByText(expectedText)).toBeInTheDocument();
+         },
       );
-      expect(screen.queryByText(/Test component/)).not.toBeInTheDocument();
+
+      test.each([
+         [
+            'withAdminRole',
+            'Test component with admin role',
+            TestComponentWithAdmin,
+         ],
+         [
+            'withEmployeeRole',
+            'Test component with employee role',
+            TestComponentWithEmployee,
+         ],
+         [
+            'withManagerRole',
+            'Test component with manager role',
+            TestComponentWithManager,
+         ],
+         [
+            'withTrainerRole',
+            'Test component with trainer role',
+            TestComponentWithTrainer,
+         ],
+         [
+            'withUserRole',
+            'Test component with user role',
+            TestComponentWithUser,
+         ],
+      ])(
+         '%s should NOT render any text.',
+         (name, expectedText, TestedComponent) => {
+            render(
+               <AuthContext.Provider
+                  value={{
+                     authState: {
+                        userInfo: {
+                           roles: [],
+                        },
+                     },
+                  }}
+               >
+                  <TestedComponent />
+               </AuthContext.Provider>,
+            );
+            expect(screen.queryByText(expectedText)).not.toBeInTheDocument();
+         },
+      );
    });
 });
