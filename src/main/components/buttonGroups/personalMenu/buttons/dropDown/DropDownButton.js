31dc5a6abede4be8d0f9b667847f45d66f3210c1
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-30 11:03:14 +0200

    Add ClickAwayListener to DropdownButton

--- src/main/components/buttonGroups/personalMenu/buttons/dropDown/DropDownButton.js
+++ src/main/components/buttonGroups/personalMenu/buttons/dropDown/DropDownButton.js
@@ -7,60 +7,71 @@ import {
    MenuList,
    MenuItem,
    Divider,
+   ClickAwayListener,
 } from '@material-ui/core';
-import { ArrowDropDown } from '@material-ui/icons';
+import { ArrowDropDown, Close } from '@material-ui/icons';
 import { LogoutButton } from 'src/main/components/buttons';
 import { useStyles } from './DropDownButton.styles';
 
 const DropDownButton = () => {
    const classes = useStyles();
    const [anchorEl, setAnchorEl] = useState(null);
+   const [open, setOpen] = useState(false);
 
    const handleClick = (event) => {
-      setAnchorEl(anchorEl ? null : event.currentTarget);
+      setOpen((prev) => !prev);
+      setAnchorEl(event.currentTarget);
    };
+
+   const handleClickAway = () => {
+      setOpen(false);
+      setAnchorEl(null);
+   };
+
    return (
-      <>
-         <Button
-            onClick={handleClick}
-            className={classes.button}
-            variant="text"
-            data-testid="dropDown-button"
-         >
-            <ArrowDropDown />
-         </Button>
-         <Popper
-            open={Boolean(anchorEl)}
-            anchorEl={anchorEl}
-            transition
-            placement="bottom-end"
-            className={classes.popper}
-            data-testId="popper"
-            modifiers={{
-               offset: {
-                  enabled: true,
-                  offset: '0, 15',
-               },
-            }}
-         >
-            {({ TransitionProps }) => (
-               // eslint-disable-next-line react/jsx-props-no-spreading
-               <Fade {...TransitionProps} timeout={350}>
-                  <div className={classes.paper}>
-                     <MenuList>
-                        <MenuItem>Moje konto</MenuItem>
-                        <Divider />
-                        <MenuItem>Pomoc</MenuItem>
-                        <MenuItem>Ustawienia prywatności</MenuItem>
-                        <MenuItem>Ustawienia wyświetlania</MenuItem>
-                        <Divider />
-                        <LogoutButton />
-                     </MenuList>
-                  </div>
-               </Fade>
-            )}
-         </Popper>
-      </>
+      <ClickAwayListener onClickAway={handleClickAway}>
+         <div>
+            <Button
+               onClick={handleClick}
+               className={classes.button}
+               variant="text"
+               data-testid="dropDown-button"
+            >
+               {open ? <Close /> : <ArrowDropDown />}
+            </Button>
+            <Popper
+               open={open}
+               anchorEl={anchorEl}
+               transition
+               placement="bottom-end"
+               className={classes.popper}
+               data-testId="popper"
+               modifiers={{
+                  offset: {
+                     enabled: true,
+                     offset: '0, 25',
+                  },
+               }}
+            >
+               {({ TransitionProps }) => (
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  <Fade {...TransitionProps} timeout={350}>
+                     <div className={classes.paper}>
+                        <MenuList>
+                           <MenuItem>Moje konto</MenuItem>
+                           <Divider />
+                           <MenuItem>Pomoc</MenuItem>
+                           <MenuItem>Ustawienia prywatności</MenuItem>
+                           <MenuItem>Ustawienia wyświetlania</MenuItem>
+                           <Divider />
+                           <LogoutButton />
+                        </MenuList>
+                     </div>
+                  </Fade>
+               )}
+            </Popper>
+         </div>
+      </ClickAwayListener>
    );
 };
 
