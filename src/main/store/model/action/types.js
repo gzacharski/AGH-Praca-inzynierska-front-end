5a46aaa75bcc5509eb0795a05a7ecc772e5270a7
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-01 21:19:16 +0200

    Remove unused files

--- src/main/store/model/action/types.js
+++
@@ -1,3 +0,0 @@
-export const ADD_USER = 'addUser';
-export const SET_USER_INFO = 'setUserInfo';
-export const SET_AVATAR = 'setAvatar';
--- src/main/store/state/action/types.js
+++
@@ -1,2 +0,0 @@
-export const TOGGLE_DRAWER = 'toggleDrawer';
-export const TOGGLE_MENU_MORE_INFO = 'toggleMenuMoreInfo';
