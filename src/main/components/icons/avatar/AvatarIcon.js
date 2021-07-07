0f74f48be29f21d11f7f77ac362e01e6070387f8
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-07 14:54:29 +0200

    Refactor AvatarIcon

--- src/main/components/icons/avatar/AvatarIcon.js
+++ src/main/components/icons/avatar/AvatarIcon.js
@@ -2,34 +2,52 @@ import React from 'react';
 import { Avatar } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { useSelector } from 'react-redux';
-import { selectStatus as selectAvatarStatus } from 'src/main/store/sliceFiles/avatarSlice';
+import {
+   selectStatus as selectAvatarStatus,
+   selectAvatar,
+} from 'src/main/store/sliceFiles/avatarSlice';
+import {
+   selectStatus as selectAccountStatus,
+   selectUserInfo,
+} from 'src/main/store/sliceFiles/accountSlice';
 import { STATUS } from 'src/main/store/status';
 import { useStyles } from './AvatarIcon.styles';
 
-export const AvatarIcon = ({ avatar, user }) => {
+export const AvatarIcon = () => {
    const classes = useStyles();
-   const status = useSelector(selectAvatarStatus);
+   const avatarStatus = useSelector(selectAvatarStatus);
+   const accountStatus = useSelector(selectAccountStatus);
+   const avatar = useSelector(selectAvatar);
+   const user = useSelector(selectUserInfo);
 
-   if (user === null || status === STATUS.IDLE || status === STATUS.LOADING) {
+   if (
+      accountStatus === STATUS.IDLE ||
+      accountStatus === STATUS.LOADING ||
+      avatarStatus === STATUS.IDLE ||
+      avatarStatus === STATUS.LOADING
+   ) {
       return (
-         <Skeleton variant="circle">
+         <Skeleton variant="circle" data-testid="avatar-skeleton">
             <Avatar className={classes.small} />
          </Skeleton>
       );
    }
 
    return (
-      <Avatar
-         alt={user && `${user?.name} ${user?.surname}`}
-         src={
-            avatar?.format &&
-            avatar?.data &&
-            `data:${avatar.format};base64, ${avatar.data}`
-         }
-         className={classes.small}
-      >
-         {user?.name[0]}
-         {user?.surname[0]}
-      </Avatar>
+      accountStatus === STATUS.SUCCEEDED && (
+         <Avatar
+            alt={user && `${user?.name} ${user?.surname}`}
+            src={
+               avatar?.format &&
+               avatar?.data &&
+               `data:${avatar.format};base64, ${avatar.data}`
+            }
+            className={classes.small}
+            data-testid="avatar"
+         >
+            {user?.name && user.name[0]}
+            {user?.surname && user.surname[0]}
+         </Avatar>
+      )
    );
 };
--- src/main/components/icons/avatar/AvatarIcon.test.js
+++ src/main/components/icons/avatar/AvatarIcon.test.js
@@ -1,25 +1,102 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { screen, render } from 'src/testUtils';
 import { testAvatar } from 'src/main/data/testData/testAvatar';
+import { STATUS } from 'src/main/store/status';
 import { AvatarIcon } from './AvatarIcon';
 
-const user = {
+const mockStore = configureStore([]);
+
+const testUser = {
    id: 'eba1df78-b210-4cd4-b4c6-a247c71e35cd',
    name: 'Krzysztof',
    surname: 'Nowalski',
+   email: null,
+   phone: null,
 };
 
+const renderAvatarIcon = (store) =>
+   render(
+      <Provider store={store}>
+         <AvatarIcon />
+      </Provider>,
+   );
+
 describe('Avatar icon', () => {
-   test('should render', () => {
-      render(<AvatarIcon avatar={testAvatar} user={user} />);
-      expect(screen.getByRole('img')).toBeInTheDocument();
-      expect(screen.getByRole('img').getAttribute('src')).toContain(
-         testAvatar.data,
-      );
+   describe('should render skeleton', () => {
+      test('while fetching data', () => {
+         const store = mockStore({
+            avatar: {
+               status: STATUS.LOADING,
+            },
+            account: {
+               status: STATUS.LOADING,
+            },
+         });
+         renderAvatarIcon(store);
+         expect(screen.getByTestId('avatar-skeleton')).toBeInTheDocument();
+      });
+
+      test('while render app for the first time', () => {
+         const store = mockStore({
+            avatar: {
+               status: STATUS.IDLE,
+            },
+            account: {
+               status: STATUS.IDLE,
+            },
+         });
+         renderAvatarIcon(store);
+         expect(screen.getByTestId('avatar-skeleton')).toBeInTheDocument();
+      });
+   });
+   describe('should render icon', () => {
+      test('should render', () => {
+         const store = mockStore({
+            avatar: {
+               image: testAvatar,
+               status: STATUS.SUCCEEDED,
+            },
+            account: {
+               userInfo: testUser,
+               status: STATUS.SUCCEEDED,
+            },
+         });
+         renderAvatarIcon(store);
+         expect(screen.getByRole('img')).toBeInTheDocument();
+         expect(screen.getByRole('img').getAttribute('src')).toContain(
+            testAvatar.data,
+         );
+      });
+
+      test('should proper when image date not provided', () => {
+         const store = mockStore({
+            avatar: {
+               status: STATUS.FAILED,
+            },
+            account: {
+               userInfo: testUser,
+               status: STATUS.SUCCEEDED,
+            },
+         });
+         renderAvatarIcon(store);
+         expect(screen.getByText(/KN/)).toBeInTheDocument();
+      });
    });
 
-   test('should proper when image date not provided', () => {
-      render(<AvatarIcon avatar={{ data: null, format: null }} user={user} />);
-      expect(screen.getByText(/KN/)).toBeInTheDocument();
+   describe('should not render avatar icon', () => {
+      test('when loadining status is failed', () => {
+         const store = mockStore({
+            avatar: {
+               status: STATUS.FAILED,
+            },
+            account: {
+               status: STATUS.FAILED,
+            },
+         });
+         renderAvatarIcon(store);
+         expect(screen.queryByTestId('avatar')).not.toBeInTheDocument();
+      });
    });
 });
