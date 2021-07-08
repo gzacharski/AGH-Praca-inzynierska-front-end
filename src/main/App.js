2275ffc46c021f8063461bdfb71e853e32cae84a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-08 15:49:54 +0200

    Wrap app with snack provider

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { SnackbarProvider } from 'notistack';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from 'src/main/auth';
 import store from 'src/main/store/store';
@@ -19,21 +20,23 @@ export default function App() {
 
    return (
       <div className={classes.root} data-testid="app-container">
-         <Router basename={process.env.PUBLIC_URL}>
-            <AuthProvider>
-               <Provider store={store}>
-                  <CssBaseline />
-                  <FilterRenderer urls={filteredUrls}>
-                     <Header />
-                  </FilterRenderer>
-                  <Navigation />
-                  <Page />
-                  <FilterRenderer urls={filteredUrls}>
-                     <Footer />
-                  </FilterRenderer>
-               </Provider>
-            </AuthProvider>
-         </Router>
+         <SnackbarProvider maxSnack={4}>
+            <Router basename={process.env.PUBLIC_URL}>
+               <AuthProvider>
+                  <Provider store={store}>
+                     <CssBaseline />
+                     <FilterRenderer urls={filteredUrls}>
+                        <Header />
+                     </FilterRenderer>
+                     <Navigation />
+                     <Page />
+                     <FilterRenderer urls={filteredUrls}>
+                        <Footer />
+                     </FilterRenderer>
+                  </Provider>
+               </AuthProvider>
+            </Router>
+         </SnackbarProvider>
       </div>
    );
 }
