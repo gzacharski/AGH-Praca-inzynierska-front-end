c0827f30aaf1900b4677d099b369878918470b38
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-09 18:26:10 +0200

    Fix test

--- src/main/components/forms/changeAccountPrivacy/ChangeAccountPrivacyForm.test.js
+++ src/main/components/forms/changeAccountPrivacy/ChangeAccountPrivacyForm.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from 'src/testUtils';
+import { AuthContext } from 'src/main/auth';
 import { ChangeAccountPrivacyForm } from './ChangeAccountPrivacyForm';
 
 describe('Change Avatar Form', () => {
    beforeEach(() => {
-      render(<ChangeAccountPrivacyForm />);
+      render(
+         <AuthContext.Provider value={{ isAuthenticated: () => true }}>
+            <ChangeAccountPrivacyForm />
+         </AuthContext.Provider>,
+      );
    });
 
    test('should have proper title', () => {
