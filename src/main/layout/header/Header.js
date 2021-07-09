a4d78022785680cf48f802d2f250e4db26821a57
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-09 20:43:12 +0200

    Change appbar position to sticky

--- src/main/layout/header/Header.js
+++ src/main/layout/header/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
    return (
       <AppBar
          aria-label="application bar"
-         position="fixed"
+         position="sticky"
          className={clsx(classes.root, {
             [classes.appBar]: authContext.isAuthenticated(),
             [classes.appBarShift]: menuIsOpen,
