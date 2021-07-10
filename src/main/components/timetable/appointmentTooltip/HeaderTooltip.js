d59b2107d442945b2b7acb5e2f17e60bd6857905
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-10 23:59:51 +0200

    Add test snackbar

--- src/main/components/timetable/appointmentTooltip/HeaderTooltip.js
+++ src/main/components/timetable/appointmentTooltip/HeaderTooltip.js
@@ -1,13 +1,30 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { AppointmentTooltip } from '@devexpress/dx-react-scheduler-material-ui';
+import { useSnackbar } from 'notistack';
 import { IconButton } from '@material-ui/core';
 import { MoreVert } from '@material-ui/icons';
 
-export const HeaderTooltip = ({ appointmentData, ...restProps }) => (
-   // eslint-disable-next-line react/jsx-props-no-spreading
-   <AppointmentTooltip.Header {...restProps} appointmentData={appointmentData}>
-      <IconButton>
-         <MoreVert />
-      </IconButton>
-   </AppointmentTooltip.Header>
-);
+export const HeaderTooltip = ({ appointmentData, ...restProps }) => {
+   const { enqueueSnackbar } = useSnackbar();
+   return (
+      <AppointmentTooltip.Header
+         {...restProps}
+         appointmentData={appointmentData}
+      >
+         <IconButton
+            onClick={() =>
+               enqueueSnackbar(JSON.stringify(appointmentData), {
+                  variant: 'success',
+                  anchorOrigin: {
+                     vertical: 'bottom',
+                     horizontal: 'right',
+                  },
+               })
+            }
+         >
+            <MoreVert />
+         </IconButton>
+      </AppointmentTooltip.Header>
+   );
+};
