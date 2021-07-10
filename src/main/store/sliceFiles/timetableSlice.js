d00486e4c8ee0b8dbfdc0e1a1cc12f5b986b719b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-10 22:55:09 +0200

    Add timetableSlice

---
+++ src/main/store/sliceFiles/timetableSlice.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import { STATUS } from '../status';
+
+const initialState = {
+   data: [
+      {
+         id: 'testid',
+         title: 'Joga',
+         startDate: '2021-07-10T09:45',
+         endDate: '2021-07-10T11:00',
+         location: 'Sala nr 1',
+         allDay: false,
+      },
+      {
+         id: 'testid2',
+         title: 'Pilates',
+         startDate: '2021-07-04T12:00',
+         endDate: '2021-07-04T13:30',
+         location: 'Sala nr 2',
+         allDay: false,
+      },
+      {
+         id: 'testid3',
+         title: 'Rowery',
+         startDate: '2021-07-09T13:00',
+         endDate: '2021-07-09T15:30',
+         allDay: false,
+      },
+      {
+         id: 'testid4',
+         title: 'TRX',
+         startDate: '2021-07-06T14:00',
+         endDate: '2021-07-06T15:00',
+         location: 'Przed budynkiem',
+         allDay: false,
+      },
+   ],
+   status: STATUS.IDLE,
+   message: null,
+   error: null,
+};
+
+export const timetableSlice = createSlice({
+   name: 'timetable',
+   initialState,
+   reducers: {
+      addAppointment(state, action) {
+         state.data.push(action.payload);
+      },
+      clearMessage(state) {
+         state.message = null;
+      },
+   },
+});
+
+export const { addAppointment, clearMessage } = timetableSlice.actions;
+
+export const selectData = (state) => state.timetable.data;
+export const selectStatus = (state) => state.timetable.status;
+export const selectMessage = (state) => state.timetable.message;
+
+export default timetableSlice.reducer;
--- src/main/store/store.js
+++ src/main/store/store.js
@@ -4,13 +4,15 @@ import accountReducer from 'src/main/store/sliceFiles/accountSlice';
 import userReducer from 'src/main/store/sliceFiles/usersSlice';
 import avatarReducer from 'src/main/store/sliceFiles/avatarSlice';
 import agreementSlice from 'src/main/store/sliceFiles/agreementSlice';
+import timetableSlice from 'src/main/store/sliceFiles/timetableSlice';
 
 export const reducer = {
    drawer: drawerReducer,
    account: accountReducer,
    users: userReducer,
    avatar: avatarReducer,
-   agreements: agreementSlice
+   agreements: agreementSlice,
+   timetable: timetableSlice,
 };
 
 export default configureStore({ reducer });
