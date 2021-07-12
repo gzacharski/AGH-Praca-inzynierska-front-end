a81f03655faad730cfcd5f2ccd9b67616722dd26
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-12 19:48:17 +0200

    Refactor TimetablePage

--- src/main/pages/timetable/TimetablePage.js
+++ src/main/pages/timetable/TimetablePage.js
@@ -1,40 +1,17 @@
 import React from 'react';
-import { Typography, Button } from '@material-ui/core';
-import { useSelector, useDispatch } from 'react-redux';
-import { PageWrapper } from 'src/main/components/utils';
+import { useSelector } from 'react-redux';
+import { PageWrapper, PageTitle } from 'src/main/components/utils';
 import { PublicTimetable } from 'src/main/components/timetable';
-import {
-   selectData,
-   addAppointment,
-} from 'src/main/store/sliceFiles/timetableSlice';
-import { useStyles } from './TimetablePage.styles';
+import { selectData } from 'src/main/store/sliceFiles/timetable/timetableSlice';
 
-export default function TimetablePage() {
-   const classes = useStyles();
+const TimetablePage = () => {
    const data = useSelector(selectData);
-   const dispatch = useDispatch();
    return (
       <PageWrapper>
-         <Typography variant="h5" className={classes.root} align="center">
-            Aktualny grafik zajęć
-         </Typography>
-         <Button
-            onClick={() =>
-               dispatch(
-                  addAppointment({
-                     id: 'brzuchy',
-                     title: 'Brzuch',
-                     startDate: '2021-07-17T12:45',
-                     endDate: '2021-07-17T14:00',
-                     location: 'Sala nr 4',
-                     allDay: false,
-                  }),
-               )
-            }
-         >
-            Test
-         </Button>
+         <PageTitle>Aktualny grafik zajęć</PageTitle>
          <PublicTimetable data={data} />
       </PageWrapper>
    );
-}
+};
+
+export default TimetablePage;
