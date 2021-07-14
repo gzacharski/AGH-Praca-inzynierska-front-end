a4646094766be480fcd4e19056c7be0fdc81b41d
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-14 23:06:59 +0200

    Add initial test data in timeTableSlice

--- src/main/store/sliceFiles/timetable/timetableSlice.js
+++ src/main/store/sliceFiles/timetable/timetableSlice.js
@@ -1,28 +1,63 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
 import { STATUS } from '../../status';
 
 const initialState = {
    data: [
       {
-         id: 'testid',
+         id: nanoid(),
          title: 'Joga',
          startDate: '2021-07-17T09:45',
          endDate: '2021-07-17T11:00',
-         location: 'Sala nr 1',
          allDay: false,
-         trainer: {
-            id: 'testTrainer',
-            name: 'Jan',
-            surname: 'Kowalski',
-            avatar: {
-               data: 'data',
-               format: 'image/jpeg',
+         location: 'Sala nr 1',
+         trainers: [
+            {
+               userId: nanoid(),
+               name: 'Toddie',
+               surname: 'Makuaa',
+               avatar:
+                  'https://tinyfac.es/data/avatars/852EC6E1-347C-4187-9D42-DF264CCF17BF-200w.jpeg',
             },
+            {
+               userId: nanoid(),
+               name: 'Toddie2',
+               surname: 'Makuaa2',
+               avatar:
+                  'https://tinyfac.es/data/avatars/852EC6E1-347C-4187-9D42-DF264CCF17BF-200w.jpeg',
+            },
+         ],
+         partipants: {
+            basicList: [
+               {
+                  userId: nanoid(),
+                  name: 'Redford',
+                  surname: 'Bowdry',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/FBEBF655-4886-455A-A4A4-D62B77DD419B-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Aditya',
+                  surname: 'Lindel',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/1C4EEDC2-FE9C-40B3-A2C9-A038873EE692-200w.jpeg',
+               },
+            ],
+            reserveList: [
+               {
+                  userId: nanoid(),
+                  name: 'Amery',
+                  surname: 'Picarello',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/26CFEFB3-21C8-49FC-8C19-8E6A62B6D2E0-200w.jpeg',
+               },
+            ],
          },
       },
       {
-         id: 'testid2',
+         id: nanoid(),
          title: 'Pilates',
          startDate: '2021-07-12T12:00',
          endDate: '2021-07-12T13:30',
@@ -30,14 +65,69 @@ const initialState = {
          allDay: false,
       },
       {
-         id: 'testid3',
+         id: nanoid(),
          title: 'Rowery',
          startDate: '2021-07-16T13:00',
          endDate: '2021-07-16T15:30',
          allDay: false,
+         partipants: {
+            basicList: [
+               {
+                  userId: nanoid(),
+                  name: 'Redford',
+                  surname: 'Bowdry',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/FBEBF655-4886-455A-A4A4-D62B77DD419B-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Aditya',
+                  surname: 'Lindel',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/1C4EEDC2-FE9C-40B3-A2C9-A038873EE692-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Toddie2',
+                  surname: 'Makuaa2',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/852EC6E1-347C-4187-9D42-DF264CCF17BF-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Redford',
+                  surname: 'Bowdry',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/FBEBF655-4886-455A-A4A4-D62B77DD419B-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Aditya',
+                  surname: 'Lindel',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/1C4EEDC2-FE9C-40B3-A2C9-A038873EE692-200w.jpeg',
+               },
+               {
+                  userId: nanoid(),
+                  name: 'Toddie2',
+                  surname: 'Makuaa2',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/852EC6E1-347C-4187-9D42-DF264CCF17BF-200w.jpeg',
+               },
+            ],
+            reserveList: [
+               {
+                  userId: nanoid(),
+                  name: 'Amery',
+                  surname: 'Picarello',
+                  avatar:
+                     'https://tinyfac.es/data/avatars/26CFEFB3-21C8-49FC-8C19-8E6A62B6D2E0-200w.jpeg',
+               },
+            ],
+         },
       },
       {
-         id: 'testid4',
+         id: nanoid(),
          title: 'TRX',
          startDate: '2021-07-14T14:00',
          endDate: '2021-07-14T15:00',
