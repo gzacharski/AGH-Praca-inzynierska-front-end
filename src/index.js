107a7fd3dd21a4f264ca3b6d149a8dae5e872959
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-15 11:08:51 +0200

    Enable msw in development mode

--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './main/App';
 import reportWebVitals from './main/reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+async function main() {
+   if (process.env.NODE_ENV === 'development') {
+      if (window.location.pathname === '/AGH-Praca-inzynierska-front-end') {
+         window.location.pathname = '/AGH-Praca-inzynierska-front-end/';
+         return;
+      }
+      // eslint-disable-next-line global-require
+      const { worker } = require('src/mocks/browser');
+      await worker.start({
+         serviceWorker: {
+            url: '/AGH-Praca-inzynierska-front-end/mockServiceWorker.js',
+         },
+      });
+   }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   ReactDOM.render(
+      <React.StrictMode>
+         <App />
+      </React.StrictMode>,
+      document.getElementById('root'),
+   );
+
+   // If you want to start measuring performance in your app, pass a function
+   // to log results (for example: reportWebVitals(console.log))
+   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+   reportWebVitals();
+}
+
+main();
