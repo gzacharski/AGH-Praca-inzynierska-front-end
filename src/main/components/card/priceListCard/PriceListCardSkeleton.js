35227da5ec5f9e3c3e1718088ed3047f8dbf6df9
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-16 17:09:49 +0200

    Add PriceListSkeleton

---
+++ src/main/components/card/priceListCard/PriceListCardSkeleton.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+   Button,
+   Card,
+   CardHeader,
+   CardContent,
+   CardActions,
+   Typography,
+} from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import { useStyles } from './PriceListCard.styles';
+
+const PriceListCardSkeleton = () => {
+   const classes = useStyles();
+
+   return (
+      <Card>
+         <CardHeader
+            titleTypographyProps={{ align: 'center' }}
+            subheaderTypographyProps={{ align: 'center' }}
+            className={classes.cardHeaderSkeleton}
+         />
+         <CardContent>
+            <div className={classes.cardContent}>
+               <div className={classes.cardPricingSkeleton}>
+                  <Typography variant="h2" align="center" color="textSecondary">
+                     <Skeleton width="80%" className={classes.textSkeleton} />
+                  </Typography>
+               </div>
+               <div className={classes.cardPricingSkeleton}>
+                  {['80%', '75%'].map((width) => (
+                     <Typography
+                        key={width}
+                        variant="h6"
+                        align="center"
+                        color="textSecondary"
+                     >
+                        <Skeleton
+                           width={width}
+                           className={classes.textSkeleton}
+                        />
+                     </Typography>
+                  ))}
+               </div>
+               <ul>
+                  {['80%', '70%', '85%', '76%', '80%'].map((width) => (
+                     <Typography
+                        key={width}
+                        component="li"
+                        variant="subtitle1"
+                        align="center"
+                        color="textSecondary"
+                     >
+                        <Skeleton
+                           width={width}
+                           className={classes.textSkeleton}
+                        />
+                     </Typography>
+                  ))}
+               </ul>
+            </div>
+         </CardContent>
+         <CardActions>
+            <Skeleton width="100%" className={classes.buttonSkeleton}>
+               <Button />
+            </Skeleton>
+         </CardActions>
+      </Card>
+   );
+};
+
+export default PriceListCardSkeleton;
