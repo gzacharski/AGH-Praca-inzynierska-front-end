0d265f78fddc9848ff4715357bdb46020391a77b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-16 22:27:15 +0200

    Refactor and add new mock handlers

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,14 +1,16 @@
 import { setupWorker } from 'msw';
-import { handlers as userGroupTimetableHandlers } from './handlers/timetableHandlers/userGroupTimetableHandler';
 import { handlers as loginHandlers } from './handlers/loginhandlers';
 import { priceListHandlers } from './handlers/priceListHandler';
 import { userEquipmentTimetableHandlers } from './handlers/timetableHandlers/userEquipmentTimetableHandler';
+import { userGroupTimetableHandlers } from './handlers/timetableHandlers/userGroupTimetableHandler';
+import { userIndividualTimetableHandler } from './handlers/timetableHandlers/userIndividualTimetableHandler';
 
 const worker = setupWorker(
-   ...userGroupTimetableHandlers,
    ...loginHandlers,
    ...priceListHandlers,
+   ...userGroupTimetableHandlers,
    ...userEquipmentTimetableHandlers,
+   ...userIndividualTimetableHandler,
 );
 
 export { worker };
