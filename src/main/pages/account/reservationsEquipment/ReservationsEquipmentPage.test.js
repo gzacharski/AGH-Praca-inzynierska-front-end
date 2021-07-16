f8249f126f29a5dcc927512d752f5f571b1491d9
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-16 22:42:17 +0200

    FIx tests

--- src/main/pages/account/reservationsEquipment/ReservationsEquipmentPage.test.js
+++ src/main/pages/account/reservationsEquipment/ReservationsEquipmentPage.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { render, screen } from 'src/testUtils';
+import { AuthContext } from 'src/main/auth';
 import ReservationsEquipmentPage from './ReservationsEquipmentPage';
 
 describe('SettingsPage', () => {
    beforeEach(() => {
-      render(<ReservationsEquipmentPage />);
+      render(
+         <AuthContext.Provider
+            value={{
+               isAuthenticated: () => true,
+               authState: { userInfo: { userId: 'test' } },
+            }}
+         >
+            <ReservationsEquipmentPage />{' '}
+         </AuthContext.Provider>,
+      );
    });
 
    test('should contain ', () => {
--- src/main/pages/account/reservationsGroupWorkout/ReservationsGroupWorkoutPage.test.js
+++ src/main/pages/account/reservationsGroupWorkout/ReservationsGroupWorkoutPage.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render, screen } from 'src/testUtils';
+import { AuthContext } from 'src/main/auth';
 import ReservationsGroupWorkout from './ReservationsGroupWorkoutPage';
 
 describe('SettingsPage', () => {
    beforeEach(() => {
-      render(<ReservationsGroupWorkout />);
+      render(
+         <AuthContext.Provider
+            value={{
+               isAuthenticated: () => true,
+               authState: { userInfo: { userId: 'test' } },
+            }}
+         >
+            <ReservationsGroupWorkout />
+         </AuthContext.Provider>,
+      );
    });
 
    test('should contain ', () => {
-      expect(screen.getByText('Twoje rezerwacje zajęć grupowych')).toBeInTheDocument();
+      expect(
+         screen.getByText('Twoje rezerwacje zajęć grupowych'),
+      ).toBeInTheDocument();
    });
 });
