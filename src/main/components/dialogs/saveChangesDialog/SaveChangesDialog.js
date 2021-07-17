a6a3b7dcc845737e96c760d4ecca77d1938f21ef
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-17 12:20:55 +0200

    Refactor SaveChangesDialog

--- src/main/components/dialogs/saveChangesDialog/SaveChangesDialog.js
+++ src/main/components/dialogs/saveChangesDialog/SaveChangesDialog.js
@@ -34,7 +34,12 @@ const SaveChangesDialog = ({
             <Button
                type={form && 'submit'}
                form={form}
-               onClick={() => !form && callback()}
+               onClick={() => {
+                  if (!form) {
+                     callback();
+                     setOpenDialog(false);
+                  }
+               }}
                className={classes.button}
             >
                {buttonText || 'Zapisz'}
