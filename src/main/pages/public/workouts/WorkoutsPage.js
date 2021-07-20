079ece88fd741279e9fd34114ac2338e2ba85e22
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-20 23:06:39 +0200

    Add connection to store from WorkoutPage and set page layout

--- src/main/pages/public/workouts/WorkoutsPage.js
+++ src/main/pages/public/workouts/WorkoutsPage.js
@@ -1,15 +1,84 @@
-import React from 'react';
-import { ShowTrainings } from 'src/main/components/gallery';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Typography, Container, Grid } from '@material-ui/core';
+import { useLocation } from 'react-router-dom';
+import { STATUS } from 'src/main/store';
 import { PageWrapper, PublicPageTitle } from 'src/main/components/utils';
+import { TrainerCardSkeleton, TrainingCard } from 'src/main/components/card';
+import { ConfirmationIcon } from 'src/main/components/icons';
+import {
+   fetchWorkoutList,
+   selectError,
+   selectMessage,
+   selectWorkouts,
+   selectStatus,
+} from 'src/main/store/sliceFiles/workoutSlice';
+import { useStyles } from './WorkoutsPage.styles';
 
-const WorkoutsPage = () => (
-   <PageWrapper>
-      <PublicPageTitle
-         header="Oferta zajęć grupowych"
-         subheader="Dobierz odpowiedni trening dla siebie"
-      />
-      <ShowTrainings />
-   </PageWrapper>
+const WorkoutCards = ({ workouts = [] }) => (
+   <Grid container spacing={4} justifyContent="center" alignItems="baseline">
+      {workouts.map((workout) => {
+         const { trainingTypeId, title, image, description, trainer } = workout;
+         return (
+            <TrainingCard
+               key={trainingTypeId}
+               image={image}
+               description={description}
+               title={title}
+               trainer={trainer}
+            />
+         );
+      })}
+   </Grid>
 );
+
+const TrainerListCardSkeleton = () => (
+   <Grid container spacing={5} justify="center" alignItems="center">
+      {[{ id: 1 }, { id: 2 }, { id: 3 }].map((card) => (
+         <TrainerCardSkeleton key={card.id} />
+      ))}
+   </Grid>
+);
+
+const WorkoutsPage = () => {
+   const classes = useStyles();
+   const dispatch = useDispatch();
+   const status = useSelector(selectStatus);
+   const workouts = useSelector(selectWorkouts);
+   const message = useSelector(selectMessage);
+   const error = useSelector(selectError);
+   const location = useLocation();
+
+   useEffect(() => {
+      if (status === STATUS.IDLE) {
+         const { search = '' } = location;
+         dispatch(fetchWorkoutList({ search }));
+      }
+   }, [status, dispatch]);
+
+   if (status === STATUS.FAILED) {
+      return (
+         <Container className={classes.container}>
+            <ConfirmationIcon onRequest={false} status={error?.status} />
+            <Typography className={classes.message}>{message}</Typography>
+         </Container>
+      );
+   }
+
+   return (
+      <PageWrapper>
+         <PublicPageTitle
+            header="Oferta zajęć grupowych"
+            subheader="Dobierz odpowiedni trening dla siebie"
+         />
+         <Container maxWidth="xl" className={{ alignItems: 'center' }}>
+            {status === STATUS.SUCCEEDED && (
+               <WorkoutCards workouts={workouts} />
+            )}
+            {status === STATUS.LOADING && <TrainerListCardSkeleton />}
+         </Container>
+      </PageWrapper>
+   );
+};
 
 export default WorkoutsPage;
