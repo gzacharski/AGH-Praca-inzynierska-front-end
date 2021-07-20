29118a8cefc6db3085eea68d1de82801b0b918a6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-20 16:27:37 +0200

    Connect trainersPage to redux store

--- src/main/pages/public/trainers/TrainersPage.js
+++ src/main/pages/public/trainers/TrainersPage.js
@@ -1,98 +1,91 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { nanoid } from 'nanoid';
+import React, { useEffect } from 'react';
+import { Typography, Container, Grid } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { STATUS } from 'src/main/store';
 import { PageWrapper, PublicPageTitle } from 'src/main/components/utils';
-import { TrainerCard } from 'src/main/components/card';
+import { TrainerCard, PriceListCardSkeleton } from 'src/main/components/card';
+import { ConfirmationIcon } from 'src/main/components/icons';
+import {
+   fetchTrainerList,
+   selectError,
+   selectMessage,
+   selectTrainers,
+   selectStatus,
+} from 'src/main/store/sliceFiles/trainerListSlice';
+import { useStyles } from './TrainerPage.styles';
 
-const testData = [
-   {
-      userId: nanoid(),
-      name: 'Redford',
-      surname: 'Bowdry',
-      images: [
-         'https://images.unsplash.com/photo-1549476464-37392f717541?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
-         'https://images.unsplash.com/photo-1613685044678-0a9ae422cf5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
-      ],
-      avatar: 'https://fwcdn.pl/ppo/71/04/57104/449672.2.jpg',
-      description: {
-         synopsis: 'Gdy potrzebujesz skorzystać jednorazowo z naszej siłowni',
-         trainings: [
-            { trainingId: nanoid(), title: 'Trening personalny' },
-            { trainingId: nanoid(), title: 'TRX' },
-         ],
-      },
-   },
-   {
-      userId: nanoid(),
-      name: 'Redford',
-      surname: 'Bowdry',
-      // images: [
-      //    'https://images.unsplash.com/photo-1613685044678-0a9ae422cf5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
-      //    'https://images.unsplash.com/photo-1549476464-37392f717541?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
-      // ],
-      avatar: 'https://fwcdn.pl/ppo/28/34/2834/449663.2.jpg',
-      description: {
-         // synopsis: 'Gdy potrzebujesz skorzystać jednorazowo z naszej siłowni',
-         full: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non lorem a dolor gravida tincidunt. Duis bibendum ligula at scelerisque pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris scelerisque tortor nec aliquam ultrices. Fusce fermentum sed nulla sed venenatis. Curabitur vehicula tristique odio, in feugiat velit accumsan sed. Ut et fermentum purus, in euismod dui. Vestibulum vehicula mollis enim vitae porttitor. Nam in bibendum nunc. Curabitur suscipit faucibus dolor. Nam a felis mattis, tincidunt libero id, feugiat tellus. Praesent neque neque, aliquet at nisl ut, tristique tincidunt tellus. Phasellus rhoncus felis nec enim malesuada, et eleifend diam vehicula. Sed posuere, leo ac placerat sagittis, augue turpis rutrum sem, at efficitur quam elit ac augue. Sed rhoncus a nibh nec tincidunt.',
-         trainings: [
-            { trainingId: nanoid(), title: 'Trening personalny' },
-            { trainingId: nanoid(), title: 'TRX' },
-            { trainingId: nanoid(), title: 'Wiosła' },
-
-            { trainingId: nanoid(), title: 'Trening personalny' },
-
-            { trainingId: nanoid(), title: 'Pilates' },
-            { trainingId: nanoid(), title: 'Trening personalny' },
-            { trainingId: nanoid(), title: 'TRX' },
-            { trainingId: nanoid(), title: 'Wiosła' },
-            { trainingId: nanoid(), title: 'Pilates' },
-         ],
-      },
-   },
-   {
-      userId: nanoid(),
-      name: 'Redford',
-      surname: 'Bowdry',
-      images: [
-         'https://images.unsplash.com/photo-1613685044678-0a9ae422cf5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
-         'https://images.unsplash.com/photo-1549476464-37392f717541?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
-      ],
-      avatar: 'https://fwcdn.pl/ppo/28/34/2834/449663.2.jpg',
-      description: {
-         synopsis: 'Gdy potrzebujesz skorzystać jednorazowo z naszej siłowni',
-         full: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non lorem a dolor gravida tincidunt. Duis bibendum ligula at scelerisque pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris scelerisque tortor nec aliquam ultrices. Fusce fermentum sed nulla sed venenatis. Curabitur vehicula tristique odio, in feugiat velit accumsan sed. Ut et fermentum purus, in euismod dui. Vestibulum vehicula mollis enim vitae porttitor. Nam in bibendum nunc. Curabitur suscipit faucibus dolor. Nam a felis mattis, tincidunt libero id, feugiat tellus. Praesent neque neque, aliquet at nisl ut, tristique tincidunt tellus. Phasellus rhoncus felis nec enim malesuada, et eleifend diam vehicula. Sed posuere, leo ac placerat sagittis, augue turpis rutrum sem, at efficitur quam elit ac augue. Sed rhoncus a nibh nec tincidunt.',
-         trainings: [
-            { trainingId: nanoid(), title: 'Trening personalny' },
-            { trainingId: nanoid(), title: 'Wiosła' },
-            { trainingId: nanoid(), title: 'Pilates' },
-         ],
-      },
-   },
-];
-
-const TrainersPage = () => (
-   <PageWrapper>
-      <PublicPageTitle
-         header="Nasi trenerzy"
-         subheader="Dowiedz się więcej o prowadzących zajęcia fitness"
-      />
-      <Grid container spacing={4} justifyContent="center" alignItems="baseline">
-         {testData.map((trainer) => {
-            const { userId, name, surname, images, avatar, description } =
-               trainer;
-            return (
-               <TrainerCard
-                  key={userId}
-                  name={name}
-                  surname={surname}
-                  images={images}
-                  avatar={avatar}
-                  description={description}
-               />
-            );
-         })}
-      </Grid>
-   </PageWrapper>
+const ShowTrainerList = ({ trainers = [] }) => (
+   <Grid container spacing={4} justifyContent="center" alignItems="baseline">
+      {trainers.map((trainer) => {
+         const { userId, name, surname, images, avatar, description } = trainer;
+         return (
+            <TrainerCard
+               key={userId}
+               name={name}
+               surname={surname}
+               images={images}
+               avatar={avatar}
+               description={description}
+            />
+         );
+      })}
+   </Grid>
 );
+
+const ShowPriceListSkeleton = () => (
+   <Grid container spacing={5} justify="center" alignItems="center">
+      {[
+         { id: 1, isSmall: true },
+         { id: 2, isSmall: false },
+         { id: 3, isSmall: true },
+      ].map((card) => (
+         <Grid item key={card.id} xs={12} sm={6} md={4} lg={3}>
+            <PriceListCardSkeleton isSmall={card.isSmall} />
+         </Grid>
+      ))}
+   </Grid>
+);
+
+const TrainersPage = () => {
+   const classes = useStyles();
+   const dispatch = useDispatch();
+   const status = useSelector(selectStatus);
+   const trainers = useSelector(selectTrainers);
+   const message = useSelector(selectMessage);
+   const error = useSelector(selectError);
+   const location = useLocation();
+
+   useEffect(() => {
+      if (status === STATUS.IDLE) {
+         const { search = '' } = location;
+         dispatch(fetchTrainerList({ search }));
+      }
+   }, [status, dispatch]);
+
+   if (status === STATUS.FAILED) {
+      return (
+         <Container className={classes.container}>
+            <ConfirmationIcon onRequest={false} status={error?.status} />
+            <Typography className={classes.message}>{message}</Typography>
+         </Container>
+      );
+   }
+
+   return (
+      <PageWrapper>
+         <PublicPageTitle
+            header="Nasi trenerzy"
+            subheader="Dowiedz się więcej o prowadzących zajęcia fitness"
+         />
+         <Container maxWidth="xl" className={{ alignItems: 'center' }}>
+            {status === STATUS.SUCCEEDED && (
+               <ShowTrainerList trainers={trainers} />
+            )}
+            {status === STATUS.LOADING && <ShowPriceListSkeleton />}
+         </Container>
+      </PageWrapper>
+   );
+};
 
 export default TrainersPage;
