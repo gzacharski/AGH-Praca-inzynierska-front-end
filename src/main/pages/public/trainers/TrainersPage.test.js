3c922062bcf9f82e45e4d1f6aba66a25e00225e7
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-20 15:40:08 +0200

    Add TrainingsCard to TrainersPage

--- src/main/pages/public/trainers/TrainersPage.test.js
+++ src/main/pages/public/trainers/TrainersPage.test.js
@@ -3,11 +3,11 @@ import { render, screen } from 'src/testUtils';
 import TrainersPage from './TrainersPage';
 
 describe('Trenings component', () => {
-   beforeEach(() => {
-      render(<TrainersPage />);
-   });
-
    test('should contain proper text', () => {
-      expect(screen.getByText(/Trenerzy/)).toBeInTheDocument();
+      render(<TrainersPage />);
+      expect(screen.getByText(/Nasi trenerzy/)).toBeInTheDocument();
+      expect(
+         screen.getByText(/Dowiedz się więcej o prowadzących zajęcia fitness/),
+      ).toBeInTheDocument();
    });
 });
