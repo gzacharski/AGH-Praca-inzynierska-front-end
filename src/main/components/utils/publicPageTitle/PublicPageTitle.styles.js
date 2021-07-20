2505f6fbc3a3697b5f7f0d0eea419fa4eaa20c1b
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-20 11:41:41 +0200

    Add PublicPageTitle component

---
+++ src/main/components/utils/publicPageTitle/PublicPageTitle.styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+   pageTitle: {
+      padding: theme.spacing(8, 0, 6),
+   },
+}));
+
+export { useStyles };
