3e22fefc5dbdaad6c1db2ba8f0ed18c2def720dd
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-20 17:58:17 +0200

    Fix trainerListHandlers

--- src/mocks/handlers/trainerListHandlers.js
+++ src/mocks/handlers/trainerListHandlers.js
@@ -38,7 +38,7 @@ export const trainerListHandlers = [
                avatar: 'https://fwcdn.pl/ppo/71/04/57104/449672.2.jpg',
                description: {
                   synopsis:
-                     'Gdy potrzebujesz skorzystać jednorazowo z naszej siłowni',
+                     'Nigdy się nie poddawaj. Na wielkie rzeczy potrzeba czasu.',
                   trainings: [
                      { trainingId: nanoid(), title: 'Trening personalny' },
                      { trainingId: nanoid(), title: 'TRX' },
@@ -71,13 +71,13 @@ export const trainerListHandlers = [
                name: 'Redford',
                surname: 'Bowdry',
                images: [
-                  'https://images.unsplash.com/photo-1613685044678-0a9ae422cf5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80',
+                  'https://images.unsplash.com/photo-1599058917765-a780eda07a3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODd8fGZpdG5lc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
                   'https://images.unsplash.com/photo-1549476464-37392f717541?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
                ],
-               avatar: 'https://fwcdn.pl/ppo/28/34/2834/449663.2.jpg',
+               avatar: 'https://fwcdn.pl/fph/10/29/1029/192816_1.2.jpg',
                description: {
                   synopsis:
-                     'Gdy potrzebujesz skorzystać jednorazowo z naszej siłowni',
+                     'Jedyną osobą, która może cię zmotywować jesteś ty sam!',
                   full: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non lorem a dolor gravida tincidunt. Duis bibendum ligula at scelerisque pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris scelerisque tortor nec aliquam ultrices. Fusce fermentum sed nulla sed venenatis. Curabitur vehicula tristique odio, in feugiat velit accumsan sed. Ut et fermentum purus, in euismod dui. Vestibulum vehicula mollis enim vitae porttitor. Nam in bibendum nunc. Curabitur suscipit faucibus dolor. Nam a felis mattis, tincidunt libero id, feugiat tellus. Praesent neque neque, aliquet at nisl ut, tristique tincidunt tellus. Phasellus rhoncus felis nec enim malesuada, et eleifend diam vehicula. Sed posuere, leo ac placerat sagittis, augue turpis rutrum sem, at efficitur quam elit ac augue. Sed rhoncus a nibh nec tincidunt.',
                   trainings: [
                      { trainingId: nanoid(), title: 'Trening personalny' },
