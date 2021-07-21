0589379ef8c492247688ae6c05d94f217be61244
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-07-21 15:17:06 +0200

    Refactor AdminRouteGroups component

--- src/main/layout/navigation/routeGroups/AdminRouteGroups.js
+++ src/main/layout/navigation/routeGroups/AdminRouteGroups.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import {
    AdminPage,
    BackupPage,
@@ -10,20 +10,31 @@ import {
    UsersPage,
 } from 'src/main/pages/private/admin';
 
-const AdminRouteGroup = () => {
-   const { path } = useRouteMatch();
-   return (
-      <Switch>
-         <Route exact path="/admin" component={AdminPage} />
-         <Route path={`${path}/backup`} component={BackupPage} />
-         <Route path={`${path}/configure`} component={ConfigurePage} />
-         <Route path={`${path}/logs`} component={LogsPage} />
-         <Route path={`${path}/manage`} component={ManagementPage} />
-         <Route path={`${path}/stats`} component={StatisticsPage} />
-         <Route path={`${path}/users`} component={UsersPage} />
-         <Redirect to="/admin" />
-      </Switch>
-   );
-};
+const AdminRouteGroup = () => (
+   <Switch>
+      <Route exact path="/admin">
+         <AdminPage />
+      </Route>
+      <Route path="/admin/backup">
+         <BackupPage />
+      </Route>
+      <Route path="/admin/configure">
+         <ConfigurePage />
+      </Route>
+      <Route path="/admin/logs">
+         <LogsPage />
+      </Route>
+      <Route path="/admin/manage">
+         <ManagementPage />
+      </Route>
+      <Route path="/admin/stats">
+         <StatisticsPage />
+      </Route>
+      <Route path="/admin/users">
+         <UsersPage />
+      </Route>
+      <Redirect to="/admin" />
+   </Switch>
+);
 
 export default AdminRouteGroup;
