d494e00ed1c6c771bc6b02c9bac5f32a947456cf
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-23 15:13:00 +0200

    Add initial managment page for admin

--- src/main/pages/private/admin/management/ManagementPage.js
+++ src/main/pages/private/admin/management/ManagementPage.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
+import { Typography, Grid, Paper } from '@material-ui/core';
+import { formatDistanceToNow } from 'date-fns';
+import { pl } from 'date-fns/locale';
 import { PageWrapper } from 'src/main/components/utils';
+import { discoveryServiceURL } from 'src/main/data/urls';
 import { useStyles } from './ManagementPage.styles';
 
-const AccountPage = () => {
-   const classes = useStyles;
+const ManagementPage = () => {
+   const classes = useStyles();
+   const [applications, setApplications] = useState([]);
+   const { enqueueSnackbar } = useSnackbar();
+
+   useEffect(() => {
+      axios
+         .get(`${discoveryServiceURL}/admin/registry`, {
+            headers: { Accept: 'application/json' },
+         })
+         .then((response) => {
+            const { application = [] } = response?.data;
+            setApplications(application);
+         })
+         .catch((error) => {
+            const { message = '' } = error?.response?.data;
+            enqueueSnackbar(message, {
+               variant: 'error',
+               anchorOrigin: {
+                  vertical: 'bottom',
+                  horizontal: 'right',
+               },
+            });
+         });
+   }, []);
+
    return (
       <PageWrapper>
          <Typography variant="h5" className={classes.root} align="center">
             Zarządzaj serwisami
          </Typography>
+         <Grid container spacing={2}>
+            {applications &&
+               applications.map((application) => {
+                  const { name = '', instance = [] } = application;
+
+                  const instances = instance.map((serviceInstance) => {
+                     const {
+                        instanceId = '',
+                        status = 'DOWN',
+                        lastUpdatedTimestamp = 0,
+                     } = serviceInstance;
+                     const lastUpdatedTimestampParsed = formatDistanceToNow(
+                        new Date(lastUpdatedTimestamp),
+                        {
+                           locale: pl,
+                           addSuffix: true,
+                        },
+                     );
+                     return (
+                        <div key={instanceId}>
+                           <Typography>{instanceId}</Typography>
+                           <div>{status}</div>
+                           <div>{lastUpdatedTimestampParsed}</div>
+                        </div>
+                     );
+                  });
+
+                  return (
+                     <Grid item key={name}>
+                        <Paper elevation={5} className={classes.paper}>
+                           <Typography>{name}</Typography>
+                           <div>{instances}</div>
+                        </Paper>
+                     </Grid>
+                  );
+               })}
+         </Grid>
       </PageWrapper>
    );
 };
 
-export default AccountPage;
+export default ManagementPage;
--- src/main/pages/private/admin/management/ManagementPage.styles.js
+++ src/main/pages/private/admin/management/ManagementPage.styles.js
@@ -1,8 +1,13 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles(({ spacing }) => ({
    root: {
       fontWeight: 'bold',
       padding: '30px',
    },
-});
+   paper: {
+      minHeight: '150px',
+      padding: spacing(3),
+      borderRadius: '10px',
+   },
+}));
