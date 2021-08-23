3ed5bc1ad9f6781ab325fdf4e6458b8d6b178a76
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-08-23 18:50:13 +0200

    Add usersSlice for admin

---
+++ src/main/store/sliceFiles/adminSlices/usersSlice.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+import {
+   createSlice,
+   createAsyncThunk,
+   createEntityAdapter,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+import { accountServiceURL } from 'src/main/data/urls';
+import { NETWORK_ERROR } from 'src/main/data/messages';
+import { requestConfig as config } from 'src/main/utils';
+import { STATUS } from '../../status';
+import { NOTISTACK } from '../../notistack';
+
+const adminUsersListAdapter = createEntityAdapter({
+   selectId: (entity) => entity.userId,
+});
+
+const initialState = adminUsersListAdapter.getInitialState({
+   data: [],
+   status: STATUS.IDLE,
+   notistack: NOTISTACK.SUCCESS,
+   message: null,
+   error: null,
+});
+
+const getNotistackVariant = (error) => {
+   const { status = 500 } = error?.response?.data;
+   let notistack = NOTISTACK.ERROR;
+   if (status === 403) notistack = NOTISTACK.WARNING;
+   if (status === 404) notistack = NOTISTACK.INFO;
+   return notistack;
+};
+
+export const fetchAdminUsersList = createAsyncThunk(
+   'adminUsersList/fetchAdminUsersList',
+   async (
+      { pageNumber = 0, pageSize = 10, token = '' },
+      { rejectWithValue },
+   ) => {
+      const url = `${accountServiceURL}/admin/users?pageNumber=${pageNumber}&pageSize=${pageSize}`;
+
+      try {
+         const response = await axios.get(url, config(token));
+         return response.data;
+      } catch (error) {
+         if (error.response === undefined) {
+            return rejectWithValue({
+               error: error?.response?.data,
+               message: NETWORK_ERROR,
+            });
+         }
+         return rejectWithValue({
+            notistack: getNotistackVariant(error),
+            error: error?.response,
+            message: error?.response?.data?.message,
+         });
+      }
+   },
+);
+
+const adminUsersSlice = createSlice({
+   name: 'adminUsersList',
+   initialState,
+   reducers: {
+      clearMessage(state, action) {
+         state.message = null;
+      },
+   },
+   extraReducers: {
+      [fetchAdminUsersList.pending]: (state, action) => {
+         state.status = STATUS.LOADING;
+      },
+      [fetchAdminUsersList.fulfilled]: (state, action) => {
+         state.status = STATUS.SUCCEEDED;
+         state.notistack = NOTISTACK.SUCCESS;
+         adminUsersListAdapter.upsertMany(state, action.payload);
+         state.error = null;
+      },
+      [fetchAdminUsersList.rejected]: (state, action) => {
+         state.status = STATUS.FAILED;
+         state.notistack = action.payload.notistack;
+         state.error = action.payload.error;
+         state.message = action.payload.message;
+      },
+   },
+});
+
+export default adminUsersSlice.reducer;
+
+export const { clearMessage } = adminUsersSlice.actions;
+
+export const { selectAll, selectById } = adminUsersListAdapter.getSelectors(
+   (state) => state.adminUsersList,
+);
+
+export const selectMessage = (state) => state.adminUsersList.message;
+export const selectStatus = (state) => state.adminUsersList.status;
+export const selectError = (state) => state.adminUsersList.error;
+export const selectNotistack = (state) => state.adminUsersList.notistack;
