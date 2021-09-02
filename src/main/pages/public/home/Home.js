da219fe3cf4be9de783935e47e1a4bee3b469cd0
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-09-02 14:20:32 +0200

    Remove unused usersSlice

--- src/main/pages/public/home/Home.js
+++ src/main/pages/public/home/Home.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Typography, Button } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import { PageWrapper } from 'src/main/components/utils';
-import {
-   selectUsersFromUser,
-   addUser,
-} from 'src/main/store/sliceFiles/usersSlice';
 import { useStyles } from './Home.styles';
 
 export default function Home() {
    const classes = useStyles();
-
-   const dispatch = useDispatch();
-
-   const userList = (userss) =>
-      userss.map((user) => <h4 key={user}>{user}</h4>);
-   const tempUsers = ['Test user'];
-   const users = useSelector(selectUsersFromUser);
 
    return (
       <PageWrapper>
          <Typography variant="h5" className={classes.root} align="center">
             Strona główna
             <br />
-            <Button
-               variant="contained"
-               color="primary"
-               onClick={() => dispatch(addUser(tempUsers[0]))}
-            >
-               Test button
-            </Button>
-            {users && userList(users)}
          </Typography>
       </PageWrapper>
    );
--- src/main/store/store.js
+++ src/main/store/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import drawerReducer from 'src/main/store/sliceFiles/drawerSlice';
 import accountReducer from 'src/main/store/sliceFiles/accountSlice';
-import userReducer from 'src/main/store/sliceFiles/usersSlice';
 import avatarReducer from 'src/main/store/sliceFiles/avatarSlice';
 import agreementSlice from 'src/main/store/sliceFiles/agreementSlice';
 import timetableSlice from 'src/main/store/sliceFiles/timetable/timetableSlice';
@@ -27,7 +26,6 @@ import userNextTrainingSlice from './sliceFiles/nextTrainingSlice';
 export const reducer = {
    drawer: drawerReducer,
    account: accountReducer,
-   users: userReducer,
    avatar: avatarReducer,
    agreements: agreementSlice,
    timetable: timetableSlice,
