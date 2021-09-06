7d60960791bf3e4956f7655accf08558dc7a7084
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-09-06 09:47:58 +0200

    Add initial TasksPage

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import App from './main/App';
 import reportWebVitals from './main/reportWebVitals';
 
 async function main() {
-   // if (process.env.NODE_ENV === 'development') {
-   //    // eslint-disable-next-line global-require
-   //    const { worker } = require('src/mocks/browser');
-   //    await worker.start({
-   //       serviceWorker: {
-   //          url: '/mockServiceWorker.js',
-   //       },
-   //    });
-   // }
+   if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line global-require
+      const { worker } = require('src/mocks/browser');
+      await worker.start({
+         serviceWorker: {
+            url: '/mockServiceWorker.js',
+         },
+      });
+   }
 
    ReactDOM.render(
       <React.StrictMode>
--- src/main/pages/private/manager/tasks/TasksPage.js
+++ src/main/pages/private/manager/tasks/TasksPage.js
@@ -1,23 +1,78 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useSnackbar } from 'notistack';
 import { useHistory } from 'react-router-dom';
-import { Typography, Button } from '@material-ui/core';
-import { PageWrapper } from 'src/main/components/utils';
-import { useStyles } from './TasksPage.styles';
+import { Button, Grid } from '@material-ui/core';
+import { PageWrapper, PageTitle } from 'src/main/components/utils';
+import { TaskItem } from 'src/main/components/grid/taskItem/TaskItem';
+
+import { STATUS } from 'src/main/store';
+import { NoAssignments } from 'src/main/components/noContent';
+import {
+   selectMessage,
+   selectStatus,
+   clearMessage,
+   selectNotistack,
+   fetchAllTaskList,
+   selectIds,
+} from 'src/main/store/sliceFiles/managerSlices/taskSlice';
+import { useAuth } from 'src/main/auth';
 
 const AccountPage = () => {
-   const classes = useStyles;
    const history = useHistory();
+   const { authState = {} } = useAuth();
+
+   const dispatch = useDispatch();
+   const status = useSelector(selectStatus);
+   const taskIds = useSelector(selectIds);
+   const message = useSelector(selectMessage);
+   const variant = useSelector(selectNotistack);
+
+   const { enqueueSnackbar } = useSnackbar();
+
+   if (message) {
+      enqueueSnackbar(message, {
+         variant,
+         anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'right',
+         },
+      });
+      dispatch(clearMessage());
+   }
+
+   useEffect(() => {
+      if (status === STATUS.IDLE) {
+         const { token = '' } = authState;
+         dispatch(fetchAllTaskList({ token }));
+      }
+   }, [status, dispatch]);
+
    return (
       <PageWrapper>
-         <Typography variant="h5" className={classes.root} align="center">
-            Zadania pracowników
-         </Typography>
-         <div>
-            <Button onClick={() => history.push('/manager/tasks/add')}>
-               Dodaj nowe zadanie
-            </Button>
-         </div>
-         <div>Przydzielone zadania</div>
+         <PageTitle>Zadania pracowników</PageTitle>
+         <Button
+            variant="outlined"
+            onClick={() => history.push('/manager/tasks/add')}
+         >
+            Dodaj nowe zadanie
+         </Button>
+         {taskIds.length !== 0 ? (
+            <Grid
+               container
+               alignItems="center"
+               justifyContent="center"
+               spacing={3}
+               direction="column"
+            >
+               {taskIds.map((id) => (
+                  <TaskItem key={id} taskId={id} />
+               ))}
+            </Grid>
+         ) : (
+            <NoAssignments />
+         )}
       </PageWrapper>
    );
 };
