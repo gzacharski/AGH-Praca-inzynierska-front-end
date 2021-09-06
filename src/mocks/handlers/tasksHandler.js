6d5a6139979f5393374b25b9f0d42486a16afb8a
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-09-06 13:19:38 +0200

    Enable task handler

--- src/mocks/handlers/tasksHandler.js
+++ src/mocks/handlers/tasksHandler.js
@@ -3,8 +3,8 @@ import { nanoid } from 'nanoid';
 import { NETWORK_ERROR } from 'src/main/data/messages';
 import { tasksServiceURL } from 'src/main/data/urls';
 
-export const locationHandler = [
-   rest.get(`${tasksServiceURL}/employee/:id/task`, (req, res, ctx) => {
+export const taskHandler = [
+   rest.get(`${tasksServiceURL}/page`, (req, res, ctx) => {
       const error = req.url.searchParams.get('error');
       if (error === 'noConnection') {
          return res(
@@ -43,11 +43,78 @@ export const locationHandler = [
                },
                title: 'Nowe zadanie',
                description: 'Opis zadania',
-               creationDate: '2021-10-01T10:00',
-               executionDate: '2021-10-10T20:00',
-               priority: 4,
+               creationDate: '2021-09-04T10:00',
+               executionDate: '2021-09-10T20:00',
+               priority: 'HIGH',
+            },
+            {
+               taskId: nanoid(),
+               assignedBy: {
+                  userId: nanoid(),
+                  name: 'Grzegorz',
+                  surname: 'Zacharski',
+                  avatar: 'https://fwcdn.pl/fph/30/06/583006/327598_1.2.jpg',
+               },
+               assignedTo: {
+                  userId: nanoid(),
+                  name: 'Wojciech',
+                  surname: 'Zacharski',
+                  avatar: 'https://fwcdn.pl/fph/90/46/119046/235875_1.2.jpg',
+               },
+               title: 'Nowe zadanie',
+               description: 'Opis zadania',
+               creationDate: '2021-09-04T10:00',
+               executionDate: '2021-09-10T20:00',
+               priority: 'MEDIUM',
             },
          ]),
+      );
+   }),
+   rest.post(`${tasksServiceURL}/manager/:id`, (req, res, ctx) => {
+      const error = req.url.searchParams.get('error');
+      if (error === 'noConnection') {
+         return res(
+            ctx.status(500),
+            ctx.delay(),
+            ctx.json({ message: NETWORK_ERROR }),
+         );
+      }
+
+      const status = req.url.searchParams.get('status');
+      if (status === 'noEquipment') {
+         return res(
+            ctx.status(404),
+            ctx.delay(1500),
+            ctx.json({ message: 'Brak przypisanych zadań' }),
+         );
+      }
+
+      return res(
+         ctx.status(200),
+         ctx.delay(),
+         ctx.json({
+            message: 'Zadanie zostało stworzone',
+            task: {
+               taskId: nanoid(),
+               assignedBy: {
+                  userId: nanoid(),
+                  name: 'Grzegorz',
+                  surname: 'Zacharski',
+                  avatar: 'https://fwcdn.pl/fph/30/06/583006/327598_1.2.jpg',
+               },
+               assignedTo: {
+                  userId: nanoid(),
+                  name: 'Wojciech',
+                  surname: 'Zacharski',
+                  avatar: 'https://fwcdn.pl/fph/90/46/119046/235875_1.2.jpg',
+               },
+               title: 'Nowe zadanie',
+               description: 'Opis zadania',
+               creationDate: '2021-09-04T10:00',
+               executionDate: '2021-09-10T20:00',
+               priority: 'HIGH',
+            },
+         }),
       );
    }),
 ];
